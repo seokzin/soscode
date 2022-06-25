90193d57c5ad0e98b20acb9b3dfc3173e665d537
seokzin <seokzin95@gmail.com>
2022-06-26 02:00:10 +0900

    feat: 할 일 목록 아이템 구현

--- TDD/feconf/src/App.jsx
+++ TDD/feconf/src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
 
 export default function App() {
   return (
     <div>
       <h1>To-do</h1>
+      <ul>
+        <li>통과를 위해 수단과 방법을 가리지 않는다</li>
+      </ul>
     </div>
   );
 }
