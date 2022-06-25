541a06b71ca5691346b6b17700f06884b207ce42
seokzin <seokzin95@gmail.com>
2022-06-26 01:49:51 +0900

    test(App): 할 일 목록 렌더링
    - 테스트 코드 커밋 메세지에 대한 고민

--- TDD/feconf/src/App.test.jsx
+++ TDD/feconf/src/App.test.jsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import { render } from '@testing-library/react';
+import { render } from "@testing-library/react";
 
-import App from './App';
+import App from "./App";
+
+describe("App", () => {
+  it("render tasks", () => {
+    const { container } = render(<App />);
+
+    expect(container).toHaveTextContent(
+      "통과를 위해 수단과 방법을 가리지 않는다"
+    );
+  });
+});
