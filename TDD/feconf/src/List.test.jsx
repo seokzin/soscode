01bca0916ad5c21d5ea9e466782138f83a56046d
seokzin <seokzin95@gmail.com>
2022-06-27 16:49:32 +0900

    test(List): 복수의 할 일 목록 렌더링

--- TDD/feconf/src/List.test.jsx
+++ TDD/feconf/src/List.test.jsx
@@ -6,12 +6,16 @@ import List from "./List";
 
 describe("List", () => {
   it("render tasks", () => {
-    const tasks = [{ id: 1, title: "통과를 위해 수단과 방법을 가리지 않는다" }];
+    const tasks = [
+      { id: 1, title: "통과를 위해 수단과 방법을 가리지 않는다" },
+      { id: 1, title: "TDD를 공부한다" },
+    ];
 
     const { container } = render(<List tasks={tasks} />);
 
     expect(container).toHaveTextContent(
       "통과를 위해 수단과 방법을 가리지 않는다"
     );
+    expect(container).toHaveTextContent("TDD를 공부한다");
   });
 });
