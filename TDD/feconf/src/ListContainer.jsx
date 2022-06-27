0d61fa715bf887246767f1822e6db8f6d2d75853
seokzin <seokzin95@gmail.com>
2022-06-28 00:55:15 +0900

    fix: tasks가 동일한 키값 보유

--- TDD/feconf/src/ListContainer.jsx
+++ TDD/feconf/src/ListContainer.jsx
@@ -5,7 +5,7 @@ import List from "./List";
 export default function ListContainer() {
   const tasks = [
     { id: 1, title: "통과를 위해 수단과 방법을 가리지 않는다" },
-    { id: 1, title: "TDD를 공부한다" },
+    { id: 2, title: "TDD를 공부한다" },
   ];
 
   return <List tasks={tasks} />;
