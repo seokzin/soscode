e35634b2acd93eb8572835505929b183bf25848d
seokzin <seokzin95@gmail.com>
2022-06-27 17:05:02 +0900

    fix: webpack dev 모드 설정
    - The 'mode' option has not been set 에러

--- TDD/feconf/webpack.config.js
+++ TDD/feconf/webpack.config.js
@@ -1,17 +1,18 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.jsx'),
+  entry: path.resolve(__dirname, "src/index.jsx"),
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: "babel-loader",
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
 };
