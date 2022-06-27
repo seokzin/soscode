29fcc12e3ebc2af0f97fc330692c4203282797d9
seokzin <seokzin95@gmail.com>
2022-06-28 00:56:47 +0900

    test(ListContainer): Redux를 통해 tasks 호출
    - jest mocking function을 통해 useSelector 선언 필요

--- TDD/feconf/src/ListContainer.test.jsx
+++ TDD/feconf/src/ListContainer.test.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
 import { render } from "@testing-library/react";
 
 import ListContainer from "./ListContainer";
 
+jest.mock("react-redux");
+
 describe("ListContainer", () => {
+  useSelector.mockImplementation((selector) =>
+    selector({
+      tasks: [
+        { id: 1, title: "통과를 위해 수단과 방법을 가리지 않는다" },
+        { id: 2, title: "TDD를 공부한다" },
+      ],
+    })
+  );
+
   it("render tasks", () => {
     const { container } = render(<ListContainer />);
 
