ddd891cf9f33d8a728f1cbe38e2fb4c8a8f82cf3
seokzin <seokzin95@gmail.com>
2022-06-28 01:05:12 +0900

    test(App): List를 ListContainer 컴포넌트로 변경
    - App 또한 jest mocking 코드가 필요 -> 코드 중복

--- TDD/feconf/src/App.test.jsx
+++ TDD/feconf/src/App.test.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
 import { render } from "@testing-library/react";
 
 import App from "./App";
 
+import tasks from "../fixtures/tasks";
+
+jest.mock("react-redux");
+
 describe("App", () => {
+  useSelector.mockImplementation((selector) =>
+    selector({
+      tasks,
+    })
+  );
+
   it("render tasks", () => {
     const { container } = render(<App />);
 
