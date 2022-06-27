6f11daeb146200990d46ffb65d448f8ab58bf224
seokzin <seokzin95@gmail.com>
2022-06-28 01:26:12 +0900

    feat(reducer): setTasks 액션 구현

--- TDD/feconf/src/reducer.js
+++ TDD/feconf/src/reducer.js
@@ -1,5 +1,10 @@
-const initialState = {
-};
+const initialState = {};
 
 export default function reducer(state = initialState, action) {
+  if (action.type === "setTasks") {
+    return {
+      ...state,
+      tasks: action.payload.tasks,
+    };
+  }
 }
