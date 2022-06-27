43e5f3bac3df095b77b3a9967e22c4b53205c16e
seokzin <seokzin95@gmail.com>
2022-06-28 01:13:35 +0900

    feat(App): dispatch 인터페이스 구현
    - 인터페이스를 미리 선언하고 실제 구현을 진행하는 것이 인상적..

--- TDD/feconf/src/App.jsx
+++ TDD/feconf/src/App.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
+import { setTasks } from "./actions";
+import tasks from "../fixtures/tasks";
 import ListContainer from "./ListContainer";
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setTasks(tasks));
+  }, []);
+
   return (
     <div>
       <h1>To-do</h1>
