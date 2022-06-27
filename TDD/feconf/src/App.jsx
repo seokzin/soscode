5db06bf5634b1d560ab9a97f8a09f6c8ddca30d8
seokzin <seokzin95@gmail.com>
2022-06-27 16:51:44 +0900

    feat(App): List 컴포넌트 연동
    - tasks 더미 배열이 하드코딩인 것은 알지만 우리의 목적은 어떻게든
      테스트를 성공시키는 것임을 명심하자

--- TDD/feconf/src/App.jsx
+++ TDD/feconf/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
+import List from "./List";
+
 export default function App() {
+  const tasks = [
+    { id: 1, title: "통과를 위해 수단과 방법을 가리지 않는다" },
+    { id: 1, title: "TDD를 공부한다" },
+  ];
+
   return (
     <div>
       <h1>To-do</h1>
-      <ul>
-        <li>통과를 위해 수단과 방법을 가리지 않는다</li>
-      </ul>
+      <List tasks={tasks} />
     </div>
   );
 }
