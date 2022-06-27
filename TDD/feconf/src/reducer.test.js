8e5fd67a7968b7226f10eb0e121e97c9b6c053b0
seokzin <seokzin95@gmail.com>
2022-06-28 01:22:49 +0900

    test(reducer): setTasks 실행시 배열에 아이템 추가
    - 아이템 존재 여부는 length !== 0 을 통해 확인

--- TDD/feconf/src/reducer.test.js
+++ TDD/feconf/src/reducer.test.js
@@ -1,5 +1,19 @@
 import reducer from "./reducer";
 
+import { setTasks } from "./actions";
+import tasks from "../fixtures/tasks";
+
 describe("reducer", () => {
-  it("", () => {});
+  describe("setTasks", () => {
+    it("changes tasks array", () => {
+      const state = reducer(
+        {
+          tasks: [],
+        },
+        setTasks(tasks)
+      );
+
+      expect(state.tasks).not.toHaveLength(0);
+    });
+  });
 });
