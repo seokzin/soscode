8cffbc799187493c75ce03227b1e3def7b1d6487
seokzin <seokzin95@gmail.com>
2022-06-28 22:54:50 +0900

    test(List): tasks 유무에 따른 테스트 시나리오 작성

--- TDD/feconf/src/List.test.jsx
+++ TDD/feconf/src/List.test.jsx
@@ -1,17 +1,32 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import List from "./List";
 
-import tasks from "../fixtures/tasks";
-
 describe("List", () => {
-  it("render tasks", () => {
-    const { container } = render(<List tasks={tasks} />);
+  context("with tasks", () => {
+    const tasks = [
+      { id: 1, title: "통과를 위해 수단과 방법을 가리지 않는다" },
+      { id: 2, title: "TDD를 공부한다" },
+    ];
 
-    expect(container).toHaveTextContent(
-      "통과를 위해 수단과 방법을 가리지 않는다"
-    );
-    expect(container).toHaveTextContent("TDD를 공부한다");
+    it("render tasks", () => {
+      const { container } = render(<List tasks={tasks} />);
+
+      expect(container).toHaveTextContent(
+        "통과를 위해 수단과 방법을 가리지 않는다"
+      );
+      expect(container).toHaveTextContent("TDD를 공부한다");
+    });
+  });
+
+  context("without tasks ", () => {
+    const tasks = [];
+
+    it("renders no tasks message", () => {
+      const { container } = render(<List tasks={tasks} />);
+
+      expect(container).toHaveTextContent("할 일이 없어요!");
+    });
   });
 });
