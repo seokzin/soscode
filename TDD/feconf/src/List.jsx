3cf835c88bc161ad3320bf15b3e87a8dd0d19223
seokzin <seokzin95@gmail.com>
2022-06-28 23:16:06 +0900

    feat(List): tasks가 없을 때 메세지 출력

--- TDD/feconf/src/List.jsx
+++ TDD/feconf/src/List.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 export default function List({ tasks }) {
+  if (tasks.length === 0) {
+    return <p>할 일이 없어요!</p>;
+  }
+
   return (
     <ul>
       {tasks.map((task) => (
