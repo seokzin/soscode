01744e369eceabd876467f1d0292d6e1994cf233
seokzin <seokzin95@gmail.com>
2022-06-30 01:03:18 +0900

    refac: 반복문 나누기
    - 반복문이 단일 기능을 하도록 분리하는 것이 효율적
    - 또한 관련 있는 코드는 모아두는 것이 좋음

--- Refactoring/ch-01/statement.js
+++ Refactoring/ch-01/statement.js
@@ -38,18 +38,21 @@ const statement = (invoice, plays) => {
   };
 
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
+
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+
   result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
 
