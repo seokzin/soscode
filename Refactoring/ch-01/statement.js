bcec8e74549a5c7e98c0740d09663d26124e77d0
seokzin <seokzin95@gmail.com>
2022-06-30 00:56:50 +0900

    refac: 함수명 명확화 및 센트값 변환 처리
    - usd로 명명하면서 함수에서 달러 변환까지 해주는 것이 더 의미적

--- Refactoring/ch-01/statement.js
+++ Refactoring/ch-01/statement.js
@@ -29,12 +29,12 @@ const statement = (invoice, plays) => {
     }
     return volumeCredits;
   };
-  const format = (aNumber) => {
+  const usd = (aNumber) => {
     new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
   };
 
   let totalAmount = 0;
@@ -45,12 +45,12 @@ const statement = (invoice, plays) => {
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
 
   return result;
