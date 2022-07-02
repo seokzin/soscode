b7fc86a624591a4f2cb6f07d261587c7282335e3
seokzin <seokzin95@gmail.com>
2022-07-02 22:11:06 +0900

    rename: 구체적인 변수명으로 변경

--- Refactoring/ch-01/statement.js
+++ Refactoring/ch-01/statement.js
@@ -21,11 +21,11 @@ const renderPlainText = (data, plays) => {
     }
     return result;
   };
-  const volumeCreditsFor = (perf) => {
+  const volumeCreditsFor = (aPerformance) => {
     let volumeCredits = 0;
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === playFor(perf).type) {
-      volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) {
+      volumeCredits += Math.floor(aPerformance.audience / 5);
     }
     return volumeCredits;
   };
