491e5316c61b4626ea3164802799431ed7feb21d
seokzin <seokzin95@gmail.com>
2022-07-02 22:25:53 +0900

    fix: 금액 계산 로직 올바르게 수정

--- Refactoring/ch-01/statement.js
+++ Refactoring/ch-01/statement.js
@@ -7,7 +7,7 @@ const renderPlainText = (data, plays) => {
       case "tragedy": // 비극
         result = 40000;
         if (aPerformance.audience > 30) {
-          result += 10000 + 500 * (aPerformance.audience - 30);
+          result += 1000 * (aPerformance.audience - 30);
         }
         break;
       case "comedy": // 희극
@@ -15,6 +15,7 @@ const renderPlainText = (data, plays) => {
         if (aPerformance.audience > 20) {
           result += 10000 + 500 * (aPerformance.audience - 20);
         }
+        result += 300 * aPerformance.audience;
         break;
       default:
         throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
