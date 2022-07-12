243cc27e5414314f8e879c4870a9ee3ef3ff0de5
seokzin <seokzin95@gmail.com>
2022-07-12 23:31:56 +0900

    refac: 인라인을 통해 단순 호출 함수 제거
    - 단계를 잘게 나누는 것이 실수 하지 않는 핵심

--- Refactoring/ch-06/02-2.js
+++ Refactoring/ch-06/02-2.js
@@ -1,14 +1,13 @@
-const gatherCustomerData = (out, aCustomer) => {
-  out.push(['name', aCustomer.name])
-  out.push(['location', aCustomer.location])
-}
-const reportLines = aCustomer => {
-  const lines = []
-  gatherCustomerData(lines, aCustomer)
-  return lines
-}
+const reportLines = (aCustomer) => {
+  const lines = [];
 
-const customerA = { name: 'roy', location: 'seoul' }
-const customerB = { name: 'jay', location: 'incheon' }
-console.log(reportLines(customerA))
-console.log(reportLines(customerB))
+  lines.push(["name", aCustomer.name]);
+  lines.push(["location", aCustomer.location]);
+
+  return lines;
+};
+
+const customerA = { name: "roy", location: "seoul" };
+const customerB = { name: "jay", location: "incheon" };
+console.log(reportLines(customerA));
+console.log(reportLines(customerB));
