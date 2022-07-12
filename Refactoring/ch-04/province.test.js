8c78b063920c5556bf6699b6eb25102310d7e490
seokzin <seokzin95@gmail.com>
2022-07-12 22:17:20 +0900

    test(province): 생산 부족분 계산 테스트

--- Refactoring/ch-04/province.test.js
+++ Refactoring/ch-04/province.test.js
@@ -1,12 +1,20 @@
-import Province from './province.js'
+import Province from "./province.js";
+import { expect } from "chai";
 
 const sampleProvinceData = () => ({
-  name: 'Asia',
+  name: "Asia",
   producers: [
-    { name: 'Byzzantium', cost: 10, production: 9 },
-    { name: 'Attalia', cost: 12, production: 10 },
-    { name: 'Sinope', cost: 10, production: 6 },
+    { name: "Byzzantium", cost: 10, production: 9 },
+    { name: "Attalia", cost: 12, production: 10 },
+    { name: "Sinope", cost: 10, production: 6 },
   ],
   demand: 30,
   price: 20,
-})
+});
+
+describe("province", () => {
+  it("shortfall", () => {
+    const asia = new Province(sampleProvinceData());
+    expect(asia.shortfall).equal(5);
+  });
+});
