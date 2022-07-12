fbdbd8eedd50b76b7fa3f5a492d7aee0932befd7
seokzin <seokzin95@gmail.com>
2022-07-12 23:05:41 +0900

    refac: 배너 출력 코드 추출

--- Refactoring/ch-06/01.js
+++ Refactoring/ch-06/01.js
@@ -1,26 +1,35 @@
-const printOwing = invoice => {
-  let outstanding = 0
+const printBanner = () => {
+  console.log("*******************");
+  console.log("***** 고객채무 *****");
+  console.log("*******************");
+};
 
-  console.log('*******************')
-  console.log('***** 고객채무 *****')
-  console.log('*******************')
+const printOwing = (invoice) => {
+  let outstanding = 0;
+
+  printBanner();
 
   for (const o of invoice.orders) {
-    outstanding += o.amount
+    outstanding += o.amount;
   }
 
-  const today = new Date()
-  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30)
+  const today = new Date();
 
-  console.log(`고객명: ${invoice.customer}`)
-  console.log(`채무액: ${outstanding}`)
-  console.log(`마감일: ${invoice.dueDate?.toLocaleString()}`)
-}
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
+
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.dueDate?.toLocaleString()}`);
+};
 
 printOwing({
-  customer: '재남',
+  customer: "석진",
   orders: [
-    { name: '사채', amount: 100 },
-    { name: '대출', amount: 1000 },
+    { name: "사채", amount: 100 },
+    { name: "대출", amount: 1000 },
   ],
-})
+});
