c6ec2bea6921bc71b9cefdda4114023f931f2c8f
seokzin <seokzin95@gmail.com>
2022-07-12 23:48:59 +0900

    refac: 로직 단위 메서드 추출
    - 객체 내부에서의 추출은 로직을 추상화하는 것에 도움

--- Refactoring/ch-06/03-2.js
+++ Refactoring/ch-06/03-2.js
@@ -1,30 +1,39 @@
 class Order {
   constructor(aRecord) {
-    this._data = aRecord
+    this._data = aRecord;
   }
   get quantity() {
-    return this._data.quantity
+    return this._data.quantity;
   }
   get itemPrice() {
-    return this._data.itemPrice
+    return this._data.itemPrice;
   }
+
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+
+  get quantityDiscount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+
+  get shipping() {
+    return Math.min(this.quantity * this.itemPrice * 0.1, 100);
+  }
+
   get price() {
-    return (
-      this.quantity * this.itemPrice -
-      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-      Math.min(this.quantity * this.itemPrice * 0.1, 100)
-    )
+    return this.basePrice - this.quantityDiscount + this.shipping;
   }
 }
 
 const orderA = new Order({
   itemPrice: 600,
   quantity: 3,
-})
+});
 const orderB = new Order({
   itemPrice: 8000,
   quantity: 2,
-})
+});
 
-console.log(orderA.price)
-console.log(orderB.price)
+console.log(orderA.price);
+console.log(orderB.price);
