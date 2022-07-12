a8dcbbf4056e1c431cb0c97dd9f28b1d3b266637
seokzin <seokzin95@gmail.com>
2022-07-12 23:27:21 +0900

    refac: 인라인을 통해 단순 호출 함수 제거

--- Refactoring/ch-06/02-1.js
+++ Refactoring/ch-06/02-1.js
@@ -1,8 +1,7 @@
-const moreThanFiveLateDeliveries = aDriver => aDriver.numberOfLateDeliveries > 5
-const rating = aDriver => (moreThanFiveLateDeliveries(aDriver) ? 2 : 1)
+const rating = (aDriver) => (aDriver.numberOfLateDeliveries > 5 ? 2 : 1);
 
-const DriverA = { name: 'A', numberOfLateDeliveries: 10 }
-const DriverB = { name: 'B', numberOfLateDeliveries: 4 }
+const DriverA = { name: "A", numberOfLateDeliveries: 10 };
+const DriverB = { name: "B", numberOfLateDeliveries: 4 };
 
-console.log(DriverA.name, rating(DriverA))
-console.log(DriverB.name, rating(DriverB))
+console.log(DriverA.name, rating(DriverA));
+console.log(DriverB.name, rating(DriverB));
