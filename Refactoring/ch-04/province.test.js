6fbced63abe07853124efbd2c7256fa3a4ee208e
seokzin <seokzin95@gmail.com>
2022-07-12 22:38:25 +0900

    refac: 표준 픽스처 선언

--- Refactoring/ch-04/province.test.js
+++ Refactoring/ch-04/province.test.js
@@ -13,13 +13,17 @@ const sampleProvinceData = () => ({
 });
 
 describe("province", () => {
+  let asia;
+
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData());
+  });
+
   it("shortfall", () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).equal(5);
   });
 
   it("profit", () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).equal(230);
   });
 });
