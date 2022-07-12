4fa85f3ef16ea60fe31d20f5228e44320d80342f
seokzin <seokzin95@gmail.com>
2022-07-12 23:37:11 +0900

    refac: 의미 있는 변수명으로 추출

--- Refactoring/ch-06/03-1.js
+++ Refactoring/ch-06/03-1.js
@@ -1,19 +1,20 @@
-const price = order => {
-  return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  )
-}
+const price = (order) => {
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount =
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+
+  return basePrice - quantityDiscount + shipping;
+};
 
 const orderA = {
   itemPrice: 600,
   quantity: 3,
-}
+};
 const orderB = {
   itemPrice: 8000,
   quantity: 2,
-}
+};
 
-console.log(price(orderA))
-console.log(price(orderB))
+console.log(price(orderA));
+console.log(price(orderB));
