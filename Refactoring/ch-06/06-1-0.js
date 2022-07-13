ae4f507c7346f7b735e4d5c9ae6ada9113fe99f2
seokzin <seokzin95@gmail.com>
2022-07-14 00:06:41 +0900

    test: 복제값 반환을 통해 원본 유지되도록 테스트

--- Refactoring/ch-06/06-1-0.js
+++ Refactoring/ch-06/06-1-0.js
@@ -1,11 +1,11 @@
 import { assert } from "chai";
+import { defaultOwner } from "./06-1-0_assign.js";
 
-let defaultOwnerData = { firstName: "마틴", lastName: "파울러" };
-
-const defaultOwner = () => defaultOwnerData;
-const setDefaultOwner = (arg) => {
-  defaultOwnerData = arg;
-};
+// let defaultOwnerData = { firstName: "마틴", lastName: "파울러" };
+// const defaultOwner = () => defaultOwnerData;
+// const setDefaultOwner = (arg) => {
+//   defaultOwnerData = arg;
+// };
 
 const owner1 = defaultOwner();
 assert.equal("파울러", owner1.lastName, "처음 값 확인");
--- Refactoring/ch-06/06-1-1_defaultOwner.js
+++
@@ -1,6 +0,0 @@
-let defaultOwnerData = { firstName: '마틴', lastName: '파울러' }
-
-export const defaultOwner = () => ({ ...defaultOwnerData })
-export const sertDefaultOwner = arg => {
-  defaultOwnerData = arg
-}
