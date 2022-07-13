3f181d70dc02dadb6725248b223ca97ed043763e
seokzin <seokzin95@gmail.com>
2022-07-13 23:49:31 +0900

    refac: 데이터 호출부를 게터 함수로 변경

--- Refactoring/ch-06/06-1-0.js
+++ Refactoring/ch-06/06-1-0.js
@@ -1,12 +1,12 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' }
+let defaultOwner = { firstName: "마틴", lastName: "파울러" };
+
+const getDefaultOwner = () => defaultOwner;
+const setDefaultOwner = (arg) => {
+  defaultOwner = arg;
+};
 
 const spaceship = {
-  owner: defaultOwner,
-}
+  owner: getDefaultOwner(),
+};
 
-defaultOwner = { firstName: '레베카', lastName: '파슨스' }
-
-const getDefaultOwner = () => defaultOwner
-const sertDefaultOwner = arg => {
-  defaultOwner = arg
-}
+defaultOwner = { firstName: "레베카", lastName: "파슨스" };
