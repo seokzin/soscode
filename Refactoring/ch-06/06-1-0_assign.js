d1f614708b78fd9a481cd15e09fff2e1b13703fc
seokzin <seokzin95@gmail.com>
2022-07-14 00:12:19 +0900

    feat: 예제 코드 분리

--- Refactoring/ch-06/06-1-0_assign.js
+++ Refactoring/ch-06/06-1-0_assign.js
@@ -1,6 +1,15 @@
+import { assert } from "chai";
+
 let defaultOwnerData = { firstName: "마틴", lastName: "파울러" };
 
 export const defaultOwner = () => ({ ...defaultOwnerData });
 export const setDefaultOwner = (arg) => {
   defaultOwnerData = arg;
 };
+
+const owner1 = defaultOwner();
+assert.equal("파울러", owner1.lastName, "처음 값 확인");
+
+const owner2 = defaultOwner();
+owner2.lastName = "파슨스";
+assert.equal("파슨스", owner1.lastName, "owner2를 변경한 후");
