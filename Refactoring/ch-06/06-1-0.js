620b0231673d84b6a7149fe4f9ebecd36b21f04e
seokzin <seokzin95@gmail.com>
2022-07-14 00:02:13 +0900

    test: 필드 값 직접 변경 테스트
    - 현재 캡슐화로는 필드 접근을 제한할 수 없음

--- Refactoring/ch-06/06-1-0.js
+++ Refactoring/ch-06/06-1-0.js
@@ -1,3 +1,5 @@
+import { assert } from "chai";
+
 let defaultOwnerData = { firstName: "마틴", lastName: "파울러" };
 
 const defaultOwner = () => defaultOwnerData;
@@ -5,10 +7,9 @@ const setDefaultOwner = (arg) => {
   defaultOwnerData = arg;
 };
 
-const spaceship = {
-  owner: defaultOwner(),
-};
+const owner1 = defaultOwner();
+assert.equal("파울러", owner1.lastName, "처음 값 확인");
 
-setDefaultOwner({ firstName: "레베카", lastName: "파슨스" });
-
-console.log(defaultOwnerData);
+const owner2 = defaultOwner();
+owner2.lastName = "파슨스";
+assert.equal("파슨스", owner1.lastName, "owner2를 변경한 후");
