6144a0ab765136b6b6b14e1a806e14a97378ad04
seokzin <seokzin95@gmail.com>
2022-07-13 23:35:31 +0900

    refac: 매개변수를 식별 코드로 받게 하여 범용성 증가

--- Refactoring/ch-06/05-2.js
+++ Refactoring/ch-06/05-2.js
@@ -1,38 +1,39 @@
 const someCustomers = [
   {
-    name: 'roy',
-    address: { state: 'MA' },
+    name: "roy",
+    address: { state: "MA" },
   },
   {
-    name: 'jay',
-    address: { state: 'CT' },
+    name: "jay",
+    address: { state: "CT" },
   },
   {
-    name: 'kay',
-    address: { state: 'ME' },
+    name: "kay",
+    address: { state: "ME" },
   },
   {
-    name: 'kai',
-    address: { state: 'NONE' },
+    name: "kai",
+    address: { state: "NONE" },
   },
   {
-    name: 'roi',
-    address: { state: 'VT' },
+    name: "roi",
+    address: { state: "VT" },
   },
   {
-    name: 'rai',
-    address: { state: 'NH' },
+    name: "rai",
+    address: { state: "NH" },
   },
   {
-    name: 'rey',
-    address: { state: 'RI' },
+    name: "rey",
+    address: { state: "RI" },
   },
-]
+];
 
-const inNewEngland = aCustomer => {
-  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(aCustomer.address.state)
-}
+const inNewEngland = (stateCode) =>
+  ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
 
-const newEnglanders = someCustomers.filter(c => inNewEngland(c))
+const newEnglanders = someCustomers.filter((c) =>
+  inNewEngland(c.address.state)
+);
 
-console.log(newEnglanders)
+console.log(newEnglanders);
