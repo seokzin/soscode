2ecc9cde75ffeb120f3be49d2cddbd85b5fa8bc3
seokzin <seokzin95@gmail.com>
2022-07-13 23:53:41 +0900

    refac: 게터 함수 get 키워드 제거
    - 게터, 세터는 인자의 차이로 충분히 구별 가능하기 때문
    - 리팩토링 이후가 useState랑 닮았다. (useState는 변수 리턴)

--- Refactoring/ch-06/06-1-0.js
+++ Refactoring/ch-06/06-1-0.js
@@ -1,12 +1,12 @@
 let defaultOwnerData = { firstName: "마틴", lastName: "파울러" };
 
-const getDefaultOwner = () => defaultOwnerData;
+const defaultOwner = () => defaultOwnerData;
 const setDefaultOwner = (arg) => {
   defaultOwnerData = arg;
 };
 
 const spaceship = {
-  owner: getDefaultOwner(),
+  owner: defaultOwner(),
 };
 
 setDefaultOwner({ firstName: "레베카", lastName: "파슨스" });
