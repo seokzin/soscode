d392dc4af34c974a2e959b37e3fa4c50473d3a96
seokzin <seokzin95@gmail.com>
2022-07-15 22:59:12 +0900

    refac: 클래스 통해 데이터 캡슐화

--- Refactoring/ch-06/09.js
+++ Refactoring/ch-06/09.js
@@ -1,30 +1,58 @@
+class Reading {
+  constructor(data) {
+    this._customer = data.customer;
+    this._quantity = data.quantity;
+    this._month = data.month;
+    this._year = data.year;
+  }
+
+  get customer() {
+    return this._customer;
+  }
+
+  get quantity() {
+    return this._quantity;
+  }
+
+  get month() {
+    return this._month;
+  }
+
+  get year() {
+    return this._year;
+  }
+}
+
 const acquireReading = () => ({
-  customer: 'ivan',
+  customer: "ivan",
   quantity: 10,
   month: 5,
   year: 2017,
-})
-const baseRate = (month, year) => year - 2000 + month
+});
+
+const baseRate = (month, year) => year - 2000 + month;
 
 const client1 = () => {
-  const aReading = acquireReading()
-  const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity
-  return baseCharge
-}
+  const aReading = acquireReading();
+  const baseCharge =
+    baseRate(aReading.month, aReading.year) * aReading.quantity;
+  return baseCharge;
+};
 
 const client2 = () => {
-  const taxThreshold = year => (year - 2000) * 0.1
-  const aReading = acquireReading()
-  const base = baseRate(aReading.month, aReading.year) * aReading.quantity
-  const taxableCharge = Math.max(0, base - taxThreshold(aReading.year))
-  return taxableCharge
-}
+  const taxThreshold = (year) => (year - 2000) * 0.1;
+  const aReading = acquireReading();
+  const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
+  const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+  return taxableCharge;
+};
 
 const client3 = () => {
-  const aReading = acquireReading()
-  const calculateBaseCharge = aReading => baseRate(aReading.month, aReading.year) * aReading.quantity
-  const basicChargeAmount = calculateBaseCharge(aReading)
-  return basicChargeAmount
-}
+  const aReading = acquireReading();
+  const calculateBaseCharge = (aReading) =>
+    baseRate(aReading.month, aReading.year) * aReading.quantity;
+  const basicChargeAmount = calculateBaseCharge(aReading);
+  return basicChargeAmount;
+};
 
-;[client1, client2, client3].forEach(c => console.log(c()))
+[client1, client2, client3].forEach((c) => console.log(c()));
