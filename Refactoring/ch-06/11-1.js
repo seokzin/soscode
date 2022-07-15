e6b863fddd34214aff3d82fb4c7425c5b7e0c1fe
seokzin <seokzin95@gmail.com>
2022-07-15 23:41:52 +0900

    refac: 배송비 계산 로직 외부 함수로 추출

--- Refactoring/ch-06/11-1.js
+++ Refactoring/ch-06/11-1.js
@@ -1,22 +1,31 @@
-import { readJSON } from '../fileController.js'
+import { readJSON } from "../fileController.js";
 
-const products = readJSON('ch6/11-products.json')
+const products = readJSON("ch-06/11-products.json");
+
 const shippingMethod = {
   discountFee: 0.1,
   feePerCase: 0.03,
   discountThreshold: 0.12,
-}
+};
+
+const applyShipping = (basePrice, shippingMethod, quantity, discount) => {
+  const shippingPerCase =
+    basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountFee
+      : shippingMethod.feePerCase;
+  const shippingCost = quantity * shippingPerCase;
+  return basePrice - discount + shippingCost;
+};
 
 const priceOrder = (product, quantity, shippingMethod) => {
-  const basePrice = product.basePrice * quantity
-  const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate
-  const shippingPerCase =
-    basePrice > shippingMethod.discountThreshold ? shippingMethod.discountFee : shippingMethod.feePerCase
-  const shippingCost = quantity * shippingPerCase
-  const price = basePrice - discount + shippingCost
-  return price
-}
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  return applyShipping(basePrice, shippingMethod, quantity, discount);
+};
 
-products.forEach(product => {
-  console.log(priceOrder(product, 10, shippingMethod))
-})
+products.forEach((product) => {
+  console.log(priceOrder(product, 10, shippingMethod));
+});
