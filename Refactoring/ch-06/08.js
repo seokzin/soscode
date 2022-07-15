7435b3c0e5c29b0d6991aa4ff01134d24a5ec674
seokzin <seokzin95@gmail.com>
2022-07-15 22:46:44 +0900

    refac: 매개변수 그룹을 객체로 교체

--- Refactoring/ch-06/08.js
+++ Refactoring/ch-06/08.js
@@ -1,17 +1,39 @@
 const station = {
-  name: 'ZB1',
+  name: "ZB1",
   readings: [
-    { temp: 47, time: '2016-11-10 09:10' },
-    { temp: 53, time: '2016-11-10 09:20' },
-    { temp: 58, time: '2016-11-10 09:30' },
-    { temp: 53, time: '2016-11-10 09:40' },
-    { temp: 51, time: '2016-11-10 09:50' },
+    { temp: 47, time: "2016-11-10 09:10" },
+    { temp: 53, time: "2016-11-10 09:20" },
+    { temp: 58, time: "2016-11-10 09:30" },
+    { temp: 53, time: "2016-11-10 09:40" },
+    { temp: 51, time: "2016-11-10 09:50" },
   ],
+};
+
+class NumberRange {
+  constructor(min, max) {
+    this._data = { min, max };
+  }
+
+  get min() {
+    return this._data.min;
+  }
+
+  get max() {
+    return this._data.max;
+  }
 }
+
 const operatingPlan = {
   temperatureFloor: 50,
   temperatureCeiling: 56,
-}
+};
 
-const readingsOutsideRange = (station, min, max) => station.readings.filter(r => r.temp < min || r.temp > max)
-console.log(readingsOutsideRange(station, operatingPlan.temperatureFloor, operatingPlan.temperatureCeiling))
+const range = new NumberRange(
+  operatingPlan.temperatureFloor,
+  operatingPlan.temperatureCeiling
+);
+
+const readingsOutsideRange = (station) =>
+  station.readings.filter((r) => r.temp < range.min || r.temp > range.max);
+
+console.log(readingsOutsideRange(station));
