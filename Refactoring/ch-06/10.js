697af41848b7711747fdcbd2cf6c015cdb2e808b
seokzin <seokzin95@gmail.com>
2022-07-15 23:12:42 +0900

    feat: 객체 복제 함수 선언
    - lodash cloneDeep 메서드 사용

--- Refactoring/ch-06/10.js
+++ Refactoring/ch-06/10.js
@@ -1,30 +1,42 @@
+import cloneDeep from "lodash/cloneDeep.js";
+
+const enrichReading = (original) => {
+  const result = cloneDeep(original);
+  return result;
+};
+
 const acquireReading = () => ({
-  customer: 'ivan',
+  customer: "ivan",
   quantity: 10,
   month: 5,
   year: 2017,
-})
-const baseRate = (month, year) => year - 2000 + month
+});
+const baseRate = (month, year) => year - 2000 + month;
 
 const client1 = () => {
-  const aReading = acquireReading()
-  const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity
-  return baseCharge
-}
+  const rawReading = acquireReading();
+  const aReading = enrichReading(rawReading);
+  const baseCharge =
+    baseRate(aReading.month, aReading.year) * aReading.quantity;
+  return baseCharge;
+};
 
 const client2 = () => {
-  const taxThreshold = year => (year - 2000) * 0.1
-  const aReading = acquireReading()
-  const base = baseRate(aReading.month, aReading.year) * aReading.quantity
-  const taxableCharge = Math.max(0, base - taxThreshold(aReading.year))
-  return taxableCharge
-}
+  const taxThreshold = (year) => (year - 2000) * 0.1;
+  const rawReading = acquireReading();
+  const aReading = enrichReading(rawReading);
+  const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
+  const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+  return taxableCharge;
+};
 
 const client3 = () => {
-  const aReading = acquireReading()
-  const calculateBaseCharge = aReading => baseRate(aReading.month, aReading.year) * aReading.quantity
-  const basicChargeAmount = calculateBaseCharge(aReading)
-  return basicChargeAmount
-}
+  const rawReading = acquireReading();
+  const aReading = enrichReading(rawReading);
+  const calculateBaseCharge = (aReading) =>
+    baseRate(aReading.month, aReading.year) * aReading.quantity;
+  const basicChargeAmount = calculateBaseCharge(aReading);
+  return basicChargeAmount;
+};
 
-;[client1, client2, client3].forEach(c => console.log(c()))
+[client1, client2, client3].forEach((c) => console.log(c()));
