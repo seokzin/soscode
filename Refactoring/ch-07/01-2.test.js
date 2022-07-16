95c076ca77ecb70eca093559ab4077232d44d4db
seokzin <seokzin95@gmail.com>
2022-07-16 22:41:06 +0900

    test: 수정된 함수로 코드 변경

--- Refactoring/ch-07/01-2.test.js
+++ Refactoring/ch-07/01-2.test.js
@@ -1,15 +1,17 @@
 import { assert } from "chai";
 import { describe, it } from "mocha";
-import { getRawDataOfCustomers, writeData, compareUsage } from "./01-2.js";
+import { getCustomerData, writeData, compareUsage } from "./01-2.js";
 
 describe("customerData", () => {
   it("initial usage of 1920 at 2016/1 to be 50", () => {
-    assert.equal(getRawDataOfCustomers()["1920"].usages["2016"]["1"], 50);
+    assert.equal(getCustomerData().usage("1920", "2016", "1"), 50);
   });
+
   it("writeData", () => {
     writeData("1920", "2016", "1", 53);
-    assert.equal(getRawDataOfCustomers()["1920"].usages["2016"]["1"], 53);
+    assert.equal(getCustomerData().usage("1920", "2016", "1"), 53);
   });
+
   it("compareUsage", () => {
     const { laterAmount, change } = compareUsage("1920", "2016", "1");
     assert.equal(laterAmount, 53);
