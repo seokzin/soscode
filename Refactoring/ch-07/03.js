71aaa248895c4d66eb461d4e909fea32fcbb1ee6
seokzin <seokzin95@gmail.com>
2022-07-16 23:15:51 +0900

    refac: 게터, 세터 메서드 선언

--- Refactoring/ch-07/03.js
+++ Refactoring/ch-07/03.js
@@ -1,14 +1,31 @@
 class Order {
-  priority = ''
+  #priority = "";
   constructor(data) {
-    this.priority = data.priority
+    this.#priority = data.priority;
+  }
+
+  get priority() {
+    return this.#priority;
+  }
+
+  set priority(aString) {
+    this.#priority = aString;
   }
 }
+
 const client1 = () => {
-  const orders = [{ priority: 'high' }, { priority: 'rush' }, { priority: 'low' }, { priority: 'normal' }].map(
-    o => new Order(o),
-  )
-  const highPriorityCount = orders.filter(o => o.priority === 'high' || o.priority === 'rush').length
-  return highPriorityCount
-}
-console.log(client1())
+  const orders = [
+    { priority: "high" },
+    { priority: "rush" },
+    { priority: "low" },
+    { priority: "normal" },
+  ].map((o) => new Order(o));
+
+  const highPriorityCount = orders.filter(
+    (o) => o.priority === "high" || o.priority === "rush"
+  ).length;
+
+  return highPriorityCount;
+};
+
+console.log(client1());
