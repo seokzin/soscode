4221eef9be182108e620cbace7875675295d0e08
seokzin <seokzin95@gmail.com>
2022-07-16 22:19:27 +0900

    refac: private 클래스 필드 선언

--- Refactoring/ch-07/01-2.js
+++ Refactoring/ch-07/01-2.js
@@ -2,30 +2,27 @@ import cloneDeep from "lodash/cloneDeep.js";
 import { readJSON } from "../fileController.js";
 
 class CustomerData {
-  _data;
+  #data;
   constructor(data) {
-    this._data = data;
+    this.#data = data;
   }
 
   usage(customerId, year, month) {
-    return this._data[customerId].usages[year][month];
+    return this.#data[customerId].usages[year][month];
   }
 
   setUsage(customerId, year, month, amount) {
-    this._data[customerId].usages[year][month] = amount;
+    this.#data[customerId].usages[year][month] = amount;
   }
 
   get rawData() {
-    return cloneDeep(this._data);
+    return cloneDeep(this.#data);
   }
 }
+
 let customerData = new CustomerData(readJSON("ch-07/01-2.json"));
 
 const getCustomerData = () => customerData;
-export const getRawDataOfCustomers = () => customerData._data;
-const setRawDataOfCustomers = (arg) => {
-  customerData = new CustomerData(arg);
-};
 
 export const writeData = (customerId, year, month, amount) => {
   getCustomerData().setUsage(customerId, year, month, amount);
