53a47730ec4fb81743a8f694c4ffe3be92e79617
seokzin <seokzin95@gmail.com>
2022-07-16 21:41:32 +0900

    refac: 클래스 형식으로 캡슐화
    - 변수, 로직까지 캡슐화 하기에 용이

--- Refactoring/ch-07/01-1.js
+++ Refactoring/ch-07/01-1.js
@@ -1,11 +1,26 @@
+class Organization {
+  constructor(data) {
+    this._data = data;
+  }
+
+  get name() {
+    return this._data.name;
+  }
+
+  set name(aString) {
+    this._data.name = aString;
+  }
+}
+
 let result = "";
-const organization = { name: "애크미 구스베리", country: "GB" };
+const organization = new Organization({
+  name: "애크미 구스베리",
+  country: "GB",
+});
 
-const getRawDataOfOrganization = () => {
-  return organization;
-};
+const getOrganization = () => organization;
 
-getRawDataOfOrganization().name = "애그머니 블루베리";
-result += `<h1>${getRawDataOfOrganization().name}</h1>`;
+getOrganization().name = "애그머니 블루베리";
+result += `<h1>${getOrganization().name}</h1>`;
 
 console.log(result);
