8069230fb50a03ee735f3daf2ad6bc7ce503ae03
seokzin <seokzin95@gmail.com>
2022-07-16 21:47:56 +0900

    refac: 클래스 데이터 객체 구체화

--- Refactoring/ch-07/01-1.js
+++ Refactoring/ch-07/01-1.js
@@ -1,14 +1,23 @@
 class Organization {
   constructor(data) {
-    this._data = data;
+    this._name = data.name;
+    this._country = data.country;
   }
 
   get name() {
-    return this._data.name;
+    return this._name;
   }
 
   set name(aString) {
-    this._data.name = aString;
+    this._name = aString;
+  }
+
+  get country() {
+    return this._country;
+  }
+
+  set country(aCountryCode) {
+    this._country = aCountryCode;
   }
 }
 
