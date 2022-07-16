dcd6138d32e55853e3ac02d924e638f9c80e87e8
seokzin <seokzin95@gmail.com>
2022-07-16 23:50:53 +0900

    refac: 세터 함수를 통해 객체 호출 간소화
    - 클래스 인라인을 위한 사전작업

--- Refactoring/ch-07/06.js
+++ Refactoring/ch-07/06.js
@@ -1,44 +1,68 @@
 class TrackingInformation {
-  _shippingCompany
-  _trackingNumber
+  _shippingCompany;
+  _trackingNumber;
+
   get shippingCompany() {
-    return this._shippingCompany
+    return this._shippingCompany;
   }
+
   set shippingCompany(arg) {
-    this._shippingCompany = arg
+    this._shippingCompany = arg;
   }
+
   get trackingNumber() {
-    return this._trackingNumber
+    return this._trackingNumber;
   }
+
   set trackingNumber(arg) {
-    this._trackingNumber = arg
+    this._trackingNumber = arg;
   }
+
   get display() {
-    return `${this.shippingCompany}: ${this.trackingNumber}`
+    return `${this.shippingCompany}: ${this.trackingNumber}`;
   }
 }
 
 class Shipment {
-  _trackingInformation
+  _trackingInformation;
   constructor() {
-    this._trackingInformation = new TrackingInformation()
+    this._trackingInformation = new TrackingInformation();
   }
+
   get trackingInfo() {
-    return this._trackingInformation.display
+    return this._trackingInformation.display;
   }
+
   get trackingInformation() {
-    return this._trackingInformation
+    return this._trackingInformation;
   }
+
   set trackingInformation(aTrackingInformation) {
-    this._trackingInformation = aTrackingInformation
+    this._trackingInformation = aTrackingInformation;
+  }
+
+  get shippingCompany() {
+    return this._trackingInformation.shippingCompany;
+  }
+
+  set shippingCompany(arg) {
+    this._trackingInformation.shippingCompany = arg;
+  }
+
+  get trackingNumber() {
+    return this._trackingInformation.trackingNumber;
+  }
+
+  set trackingNumber(arg) {
+    this._trackingInformation.trackingNumber = arg;
   }
 }
 
 const client1 = () => {
-  const aShipment = new Shipment()
-  const vendor = { name: 'A-SHIP', number: '010-1234-5678' }
-  aShipment.trackingInformation.shippingCompany = vendor.name
-  aShipment.trackingInformation.trackingNumber = vendor.number
-  return aShipment.trackingInfo
-}
-console.log(client1())
+  const aShipment = new Shipment();
+  const request = { vendor: "A-SHIP", number: "010-1234-5678" };
+  aShipment.shippingCompany = request.vendor;
+  aShipment.trackingNumber = request.number;
+  return aShipment.trackingInfo;
+};
+console.log(client1());
