b3bcced14ba19106f97edbb74f948678ea2982ef
seokzin <seokzin95@gmail.com>
2022-07-17 22:36:06 +0900

    refac: 클래스 역할 분리
    - SRP 관점에서는 와닿지만 실전 활용도는 모르겠다
    - (예제의 코드 누락이 많은 파트)

--- Refactoring/ch-08/01-2.js
+++ Refactoring/ch-08/01-2.js
@@ -1,28 +1,50 @@
-class Account {
-  daysOverdrawn
-  type = { isPremium: false }
-  constructor(daysOverdrawn, type) {
-    this.daysOverdrawn = daysOverdrawn
-    this.type = type
+class AccountType {
+  isPremium;
+  constructor(type) {
+    this.isPremium = type.isPremium;
   }
-  get bankCharge() {
-    let result = 4.5
-    if (this.daysOverdrawn > 0) result += this.overdraftCharge
-    return result
-  }
-  get overdraftCharge() {
-    // 마통이자
-    if (this.type.isPremium) {
-      const baseCharge = 10
-      if (this.daysOverdrawn <= 7) return baseCharge
-      return baseCharge + (this.daysOverdrawn - 7) * 0.85
+
+  overdraftCharge(daysOverdrawn) {
+    if (this.isPremium) {
+      const baseCharge = 10;
+      if (daysOverdrawn <= 7) return baseCharge;
+      return baseCharge + (daysOverdrawn - 7) * 0.85;
     }
-    return this.daysOverdrawn * 1.75
+
+    return daysOverdrawn * 1.75;
   }
 }
 
-const loan = new Account(10, { isPremium: true })
-const repo = new Account(10, { isPremium: false })
+class Account {
+  daysOverdrawn;
+  type = { isPremium: false };
+  constructor(daysOverdrawn, type) {
+    this.daysOverdrawn = daysOverdrawn;
+    this.type = new AccountType(type);
+  }
 
-console.log({ name: 'loan', charge: loan.bankCharge, overdraftCharge: loan.overdraftCharge })
-console.log({ name: 'repo', charge: repo.bankCharge, overdraftCharge: repo.overdraftCharge })
+  get bankCharge() {
+    let result = 4.5;
+    if (this.daysOverdrawn > 0) result += this.overdraftCharge;
+    return result;
+  }
+
+  get overdraftCharge() {
+    return this.type.overdraftCharge(this.daysOverdrawn);
+  }
+}
+
+const loan = new Account(10, { isPremium: true });
+const repo = new Account(10, { isPremium: false });
+
+console.log({
+  name: "loan",
+  charge: loan.bankCharge,
+  overdraftCharge: loan.overdraftCharge,
+});
+
+console.log({
+  name: "repo",
+  charge: repo.bankCharge,
+  overdraftCharge: repo.overdraftCharge,
+});
