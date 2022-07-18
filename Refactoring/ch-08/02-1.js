c785286964ca8b118b1b6aa8cd827797f5b3885d
seokzin <seokzin95@gmail.com>
2022-07-19 00:09:23 +0900

    refac: 할인율 필드를 하위 클래스로 이동

--- Refactoring/ch-08/02-1.js
+++ Refactoring/ch-08/02-1.js
@@ -1,32 +1,41 @@
 class Customer {
-  _name
-  _discountRate
-  _contract
-
+  _name;
+  _contract;
   constructor(name, discountRate) {
-    this._name = name
-    this._discountRate = discountRate
-    this._contract = new CustomerContract(new Date())
+    this._name = name;
+    this._contract = new CustomerContract(new Date(), discountRate);
   }
+
   get discountRate() {
-    return this._discountRate
+    return this._contract.discountRate;
   }
+
   becomePreferred() {
-    this._discountRate += 0.03
-    // do other stuff
+    this._contract.discountRate += 0.03;
   }
+
   applyDiscount(amount) {
-    return amount.subtract(amount.multiply(this._discountRate))
+    return amount.subtract(amount.multiply(this._contract.discountRate));
   }
 }
 
 class CustomerContract {
-  _startDate
-  constructor(startDate) {
-    this._startDate = startDate
+  _startDate;
+  _discountRate;
+  constructor(startDate, discountRate) {
+    this._startDate = startDate;
+    this._discountRate = discountRate;
+  }
+
+  get discountRate() {
+    return this._discountRate;
+  }
+
+  set discountRate(number) {
+    this._discountRate = number;
   }
 }
 
-const customer1 = new Customer('재남', 0.1)
-customer1.becomePreferred()
-console.log(customer1.discountRate)
+const customer1 = new Customer("재남", 0.1);
+customer1.becomePreferred();
+console.log(customer1.discountRate);
