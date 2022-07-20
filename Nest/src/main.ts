416c410d8cfe69870a9e629183e53d7be96783fa
seokzin <seokzin95@gmail.com>
2022-07-20 15:26:24 +0900

    feat: Swagger 연동
    - 핫 리로딩 X (Swagger 수정시 서버 리셋 필요)

--- Nest/src/main.ts
+++ Nest/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('movies')
+    .setDescription('Nest.js project')
+    .setVersion('1.0')
+    .addTag('movie')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
