6b7b453ab573bd8cc43d7a4fa2c10ad199477e8f
seokzin <seokzin95@gmail.com>
2022-07-25 00:22:36 +0900

    test: App 최초 접근
    - Testing DB에 대한 관례적 접근 (빈 배열 테스트)

--- Nest/src/app.controller.ts
+++ Nest/src/app.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('app')
-export class AppController {}
+@Controller('')
+export class AppController {
+  @Get()
+  home() {
+    return 'Welcome to my Movie API';
+  }
+}
--- Nest/test/app.e2e-spec.ts
+++ Nest/test/app.e2e-spec.ts
@@ -19,6 +19,10 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
+  });
+
+  it('/movies (GET)', () => {
+    return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
   });
 });
