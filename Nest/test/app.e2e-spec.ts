4130b60fe66f59c1e73bbf0cf7e06cf9933fa4fc
seokzin <seokzin95@gmail.com>
2022-07-25 00:54:43 +0900

    test: movies e2e (POST validation)
    - 200: OK
    - 201: Created
    - 400: Bad Request
    - 404: Not Found

--- Nest/test/app.e2e-spec.ts
+++ Nest/test/app.e2e-spec.ts
@@ -34,7 +34,7 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
 
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -43,6 +43,18 @@ describe('AppController (e2e)', () => {
           genres: ['test'],
         })
         .expect(201);
+    });
+
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test Movie',
+          year: 2022,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
     });
 
     it('DELETE', () => {
@@ -59,14 +71,14 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer()).get('/movies/999').expect(404);
     });
 
-    it('PATCH', () => {
+    it('PATCH 200', () => {
       return request(app.getHttpServer())
         .patch('/movies/1')
         .send({ title: 'Updated Test' })
         .expect(200);
     });
 
-    it('DELETE', () => {
+    it('DELETE 200', () => {
       return request(app.getHttpServer()).delete('/movies/1').expect(200);
     });
   });
