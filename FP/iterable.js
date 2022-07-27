448df9a5c68c29296a85f78031f9c35f753e967d
seokzin <seokzin95@gmail.com>
2022-07-27 22:11:25 +0900

    feat: well-formed iterable 구조로 변경

--- FP/iterable.js
+++ FP/iterable.js
@@ -8,6 +8,15 @@ const iterable = {
       next() {
         return i === 0 ? { done: true } : { value: i--, done: false };
       },
+      [Symbol.iterator]() {
+        return this;
+      },
     };
   },
 };
+
+let iterator = iterable[Symbol.iterator]();
+
+// iterable === iterable[Symbol.iterator]() → Well-formed iterable
+for (const a of iterable) log(a);
+for (const a of iterator) log(a);
