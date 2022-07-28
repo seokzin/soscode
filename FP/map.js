00498e9ea587b310eb5e33433ecef2c345f05c94
seokzin <seokzin95@gmail.com>
2022-07-28 23:37:46 +0900

    feat: 일반 map 함수 구현

--- FP/map.js
+++ FP/map.js
@@ -1,12 +1,21 @@
 import { curry } from "./curry.js";
 
-export const map = curry(function* (f, iter) {
+export const map = (f, iter) => {
+  let res = [];
+  for (const a of iter) {
+    res.push(f(a));
+  }
+  return res;
+};
+
+export const l_map = curry(function* (f, iter) {
   for (const a of iter) {
     yield f(a);
   }
 });
 
-const generator = map((x) => x ** 2)([1, 2, 3]);
+const array = map((x) => x ** 2, [1, 2, 3]);
+const generator = l_map((x) => x ** 2)([1, 2, 3]);
 
 // console.log(generator); // Object [Generator] {}
 // console.log(generator.next()); // { value: 1, done: false }
