f30e5fe301421dde6ce873bdc036fd586d2e6312
seokzin <seokzin95@gmail.com>
2022-07-28 23:39:33 +0900

    feat: 일반 filter 함수 구현

--- FP/filter.js
+++ FP/filter.js
@@ -1,12 +1,21 @@
 import { curry } from "./curry.js";
 
-export const filter = curry(function* (f, iter) {
+export const filter = (f, iter) => {
+  let res = [];
+  for (const a of iter) {
+    if (f(a)) res.push(a);
+  }
+  return res;
+};
+
+export const l_filter = curry(function* (f, iter) {
   for (const a of iter) {
     if (f(a)) yield a;
   }
 });
 
-const generator = filter((x) => x > 2)([1, 2, 3, 4, 5]);
+const array = filter((x) => x > 2, [1, 2, 3, 4]);
+const generator = l_filter((x) => x > 2)([1, 2, 3, 4, 5]);
 
 // console.log(generator); // Object [Generator] {}
 // console.log(generator.next()); // { value: 3, done: false }
