75e958f405b1ceed72de7268481541d705a682d1
seokzin <seokzin95@gmail.com>
2022-07-28 23:55:46 +0900

    feat: reduce + map + filter 예제 추가
    - 평가는 역순(상향식)으로 이루어짐
    - 평가 후 리턴 값으로 대체되는 과정

--- FP/reduce.js
+++ FP/reduce.js
@@ -1,4 +1,7 @@
+import { map } from "./map.js";
+import { filter } from "./filter.js";
 import { curry } from "./curry.js";
+import { log, add } from "./utils.js";
 
 export const reduce = curry(function (f, acc, iter) {
   if (!iter) {
@@ -13,5 +16,33 @@ export const reduce = curry(function (f, acc, iter) {
   return acc;
 });
 
-console.log(reduce((a, b) => a + b, 0, [1, 2, 3, 4, 5])); // 15
-console.log(reduce((a, b) => a + b, [1, 2, 3, 4, 5])); // 15
+// log(reduce((a, b) => a + b, 0, [1, 2, 3, 4, 5])); // 15
+// log(reduce((a, b) => a + b, [1, 2, 3, 4, 5])); // 15
+
+// const products = [
+//   { name: "상품A", price: 15000 },
+//   { name: "상품B", price: 20000 },
+//   { name: "상품C", price: 15000 },
+//   { name: "상품D", price: 30000 },
+//   { name: "상품E", price: 25000 },
+// ];
+
+// log(
+//   reduce(
+//     add,
+//     map(
+//       (p) => p.price,
+//       filter((p) => p.price < 20000, products)
+//     )
+//   )
+// ); // 30000
+
+// log(
+//   reduce(
+//     add,
+//     filter(
+//       (n) => n < 20000,
+//       map((p) => p.price, products)
+//     )
+//   )
+// ); // 30000
