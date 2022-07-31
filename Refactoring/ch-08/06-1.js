7d5e98944d36a06fa16a3db7ac43d056b2bea9bd
seokzin <seokzin95@gmail.com>
2022-07-31 21:00:47 +0900

    refac: 관련된 코드 모아두기

--- Refactoring/ch-08/06-1.js
+++ Refactoring/ch-08/06-1.js
@@ -1,14 +1,16 @@
-const retreivePricingPlan = () => ({
+const retrievePricingPlan = () => ({
   base: 50, // 기본요금
   unit: 100, // 제품 하나당 가격
   discountThreshold: 7, // 몇개 이상부터 할인 적용할지 (8개: 0개 할인. 8개: 1개 할인. 10개: 3개 할인)
   discountFactor: 20, // 개당 할인액
-})
-const retreiveOrder = () => ({
+});
+
+const retrieveOrder = () => ({
   units: 8,
   isRepeat: true, // 반복주문시 추가할인
-})
-const chargeOrder = charge => console.log(charge)
+});
+
+const chargeOrder = (charge) => console.log(charge);
 
 // 반복주문, 8개 주문시:
 // 기본요금50 + 주문액 100 * 8 = 850
@@ -16,16 +18,20 @@ const chargeOrder = charge => console.log(charge)
 // 반복주문에 대한 추가할인액 20
 // result: 810
 
-const pricingPlan = retreivePricingPlan()
-const order = retreiveOrder()
-const baseCharge = pricingPlan.base
-let charge
-const chargePerUnit = pricingPlan.unit
-const units = order.units
-let discount
-charge = baseCharge + units * chargePerUnit
-let discountableUnits = Math.max(units - pricingPlan.discountThreshold, 0)
-discount = discountableUnits * pricingPlan.discountFactor
-if (order.isRepeat) discount += 20
-charge = charge - discount
-chargeOrder(charge)
+const pricingPlan = retrievePricingPlan();
+const baseCharge = pricingPlan.base;
+const chargePerUnit = pricingPlan.unit;
+
+const order = retrieveOrder();
+const units = order.units;
+
+let discountableUnits = Math.max(units - pricingPlan.discountThreshold, 0);
+
+let discount;
+discount = discountableUnits * pricingPlan.discountFactor;
+
+if (order.isRepeat) discount += 20;
+let charge;
+charge = baseCharge + units * chargePerUnit;
+charge = charge - discount;
+chargeOrder(charge);
