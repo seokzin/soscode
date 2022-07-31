75d0337ae8713c5ff78b23a866ef78adbdbec793
seokzin <seokzin95@gmail.com>
2022-07-31 21:18:08 +0900

    refac: 반복 로직을 분리하여 전역 함수로 추출
    - 그 과정에서 내부 변수명을 result로 일반화

--- Refactoring/ch-08/07.js
+++ Refactoring/ch-08/07.js
@@ -4,15 +4,25 @@ const PEOPLE = [
   { age: 24, salary: 2800 },
   { age: 37, salary: 4600 },
   { age: 27, salary: 3200 },
-]
+];
 
-const getInfos = people => {
-  let youngest = people[0] ? people[0].age : Infinity
-  let totalSalary = 0
+const youngestAge = (people) => {
+  let result = people[0] ? people[0].age : Infinity;
   for (const p of people) {
-    if (p.age < youngest) youngest = p.age
-    totalSalary += p.salary
+    if (p.age < result) result = p.age;
   }
-  return `최연소: ${youngest}, 총급여: ${totalSalary}`
-}
-console.log(getInfos(PEOPLE))
+  return result;
+};
+
+const totalSalary = (people) => {
+  let result = 0;
+  for (const p of people) {
+    result += p.salary;
+  }
+  return result;
+};
+
+const getInfos = (people) =>
+  `최연소: ${youngestAge(people)}, 총급여: ${totalSalary(people)}`;
+
+console.log(getInfos(PEOPLE));
