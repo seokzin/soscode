a7d575007638f5beaf35beff885009b34389e010
seokzin <seokzin95@gmail.com>
2022-07-31 21:13:44 +0900

    refac: 조건문을 삼항 연산자로 표현

--- Refactoring/ch-08/06-2.js
+++ Refactoring/ch-08/06-2.js
@@ -3,12 +3,11 @@ const availableResources = [];
 const allocatedResources = [];
 
 const func = () => {
-  let result;
-  if (availableResources.length === 0) {
-    result = createResource();
-  } else {
-    result = availableResources.pop();
-  }
+  const result =
+    availableResources.length === 0
+      ? createResource()
+      : availableResources.pop();
+
   allocatedResources.push(result);
 
   return result;
