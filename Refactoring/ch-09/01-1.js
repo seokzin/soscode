16dec56bb762ddb99d56ed1bf05d5f86a7a03e87
seokzin <seokzin95@gmail.com>
2022-08-01 23:32:26 +0900

    refac: 중복된 변수명 분리

--- Refactoring/ch-09/01-1.js
+++ Refactoring/ch-09/01-1.js
@@ -3,20 +3,25 @@ const scenario = {
   secondaryForce: 10,
   mass: 10,
   delay: 40,
-}
+};
 
 const distanceTravelled = (scenario, time) => {
-  let result
-  let acc = scenario.primaryForce / scenario.mass // (a = F / m)
-  let primaryTime = Math.min(time, scenario.delay)
-  result = 0.5 * acc * primaryTime ** 2
-  let secondaryTime = time - scenario.delay
-  if (secondaryTime > 0) {
-    let primaryVelocity = acc * scenario.delay
-    acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass
-    result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime ** 2
-  }
-  return result
-}
+  let result;
+  const primaryAcceleration = scenario.primaryForce / scenario.mass; // (a = F / m)
+  const primaryTime = Math.min(time, scenario.delay);
+  result = 0.5 * primaryAcceleration * primaryTime ** 2;
 
-console.log(distanceTravelled(scenario, 100))
+  const secondaryTime = time - scenario.delay;
+  if (secondaryTime > 0) {
+    const primaryVelocity = primaryAcceleration * scenario.delay;
+    const secondaryAcceleration =
+      (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+    result +=
+      primaryVelocity * secondaryTime +
+      0.5 * secondaryAcceleration * secondaryTime ** 2;
+  }
+
+  return result;
+};
+
+console.log(distanceTravelled(scenario, 100));
