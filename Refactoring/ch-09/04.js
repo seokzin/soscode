587e44ead4fdda7aa7c0b0d614f957d313c27912
seokzin <seokzin95@gmail.com>
2022-08-02 00:14:30 +0900

    refac: 객체 불변화
    - 불변화된 객체는 다루기 안전하다
    - 하지만 예시는 장점이 드러나기엔 아쉬움

--- Refactoring/ch-09/04.js
+++ Refactoring/ch-09/04.js
@@ -1,40 +1,52 @@
 class TelephoneNumber {
-  #areaCode
-  #number
+  #areaCode;
+  #number;
+  constructor(areaCode, number) {
+    this.#areaCode = areaCode;
+    this.#number = number;
+  }
+
   get areaCode() {
-    return this.#areaCode
+    return this.#areaCode;
   }
+
   set areaCode(arg) {
-    this.#areaCode = arg
+    this.#areaCode = arg;
   }
+
   get number() {
-    return this.#number
+    return this.#number;
   }
+
   set number(arg) {
-    this.#number = arg
+    this.#number = arg;
   }
 }
 
 class Person {
-  #telephoneNumber
+  #telephoneNumber;
   constructor() {
-    this.#telephoneNumber = new TelephoneNumber()
+    this.#telephoneNumber = new TelephoneNumber();
   }
+
   get officeAreaCode() {
-    return this.#telephoneNumber.areaCode
+    return this.#telephoneNumber.areaCode;
   }
+
   set officeAreaCode(arg) {
-    this.#telephoneNumber.areaCode = arg
+    this.#telephoneNumber = new TelephoneNumber(arg, this.officeNumber);
   }
+
   get officeNumber() {
-    return this.#telephoneNumber.number
+    return this.#telephoneNumber.number;
   }
+
   set officeNumber(arg) {
-    this.#telephoneNumber.number = arg
+    this.#telephoneNumber = new TelephoneNumber(this.officeAreaCode, arg);
   }
 }
 
-const p = new Person()
-p.officeAreaCode = '312'
-p.officeNumber = '555-0142'
-console.log(p.officeAreaCode, p.officeNumber)
+const p = new Person();
+p.officeAreaCode = "312";
+p.officeNumber = "555-0142";
+console.log(p.officeAreaCode, p.officeNumber);
