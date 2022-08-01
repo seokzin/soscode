7477b64084a7df9aa688cc43d81c335b34bfaf9d
seokzin <seokzin95@gmail.com>
2022-08-01 23:48:21 +0900

    refac: 중첩된 변수명 변경

--- Refactoring/ch-09/01-2.js
+++ Refactoring/ch-09/01-2.js
@@ -1,8 +1,10 @@
 const discount = (inputValue, quantity) => {
-  if (inputValue > 50) inputValue = inputValue - 2
-  if (quantity > 100) inputValue = inputValue - 1
-  return inputValue
-}
-console.log(discount(40, 90))
-console.log(discount(70, 90))
-console.log(discount(70, 110))
+  let result = inputValue;
+  if (inputValue > 50) result -= 2;
+  if (quantity > 100) result -= 1;
+  return result;
+};
+
+console.log(discount(40, 90));
+console.log(discount(70, 90));
+console.log(discount(70, 110));
