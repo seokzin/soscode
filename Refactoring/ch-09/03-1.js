3fd73d27b518d4bfc1719b32d0312b3c71336445
seokzin <seokzin95@gmail.com>
2022-08-01 23:55:38 +0900

    refac: 불필요한 데이터 변경 로직 제거
    - adjustment 계산 메서드에 production까지 제어중
    - 다중 책임 상태

--- Refactoring/ch-09/03-1.js
+++ Refactoring/ch-09/03-1.js
@@ -1,17 +1,21 @@
 class ProductionPlan {
-  #production = 0
-  #adjustments = []
+  #adjustments = [];
+
   get production() {
-    return this.#production
+    return this.calculatedProduction;
   }
+
+  get calculatedProduction() {
+    return this.#adjustments.reduce((sum, a) => sum + a.amount, 0);
+  }
+
   applyAdjustment(anAdjustment) {
-    this.#adjustments.push(anAdjustment)
-    this.#production += anAdjustment.amount
+    this.#adjustments.push(anAdjustment);
   }
 }
 
-const products = new ProductionPlan()
-products.applyAdjustment({ name: '사과', amount: 10 })
-products.applyAdjustment({ name: '바나나', amount: 20 })
+const products = new ProductionPlan();
+products.applyAdjustment({ name: "사과", amount: 10 });
+products.applyAdjustment({ name: "바나나", amount: 20 });
 
-console.log(products.production)
+console.log(products.production);
