93ffe4c79692ec13198fc258025b842110211978
seokzin <seokzin95@gmail.com>
2022-08-02 08:51:45 +0900

    refac: 조건절을 함수로 분리
    - 의도를 명확히 드러내기 위해

--- Refactoring/ch-10/01.js
+++ Refactoring/ch-10/01.js
@@ -1,16 +1,23 @@
-import dayjs from 'dayjs'
+import dayjs from "dayjs";
 
 const plan = {
-  summerStart: dayjs('2021-07-01'),
-  summerEnd: dayjs('2021-08-31'),
+  summerStart: dayjs("2021-07-01"),
+  summerEnd: dayjs("2021-08-31"),
   summerRate: 1000,
   regularRate: 1100,
   regularServiceCharge: 100,
-}
+};
 
-const getCharge = (quantity, aDate) => {
-  if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd)) return quantity * plan.summerRate
-  return quantity * plan.regularRate + plan.regularServiceCharge
-}
-console.log(getCharge(10, dayjs('2021-06-29')))
-console.log(getCharge(10, dayjs('2021-08-15')))
+const summer = (aDate) =>
+  !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+
+const summerCharge = (quantity) => quantity * plan.summerRate;
+
+const regularCharge = (quantity) =>
+  quantity * plan.regularRate + plan.regularServiceCharge;
+
+const getCharge = (quantity, aDate) =>
+  summer(aDate) ? summerCharge(quantity) : regularCharge(quantity);
+
+console.log(getCharge(10, dayjs("2021-06-29")));
+console.log(getCharge(10, dayjs("2021-08-15")));
