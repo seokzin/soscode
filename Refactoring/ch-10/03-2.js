9fdf2a7d62521a24f0bcf5ede0f0001a07599d26
seokzin <seokzin95@gmail.com>
2022-08-02 09:21:03 +0900

    refac: 조건식 통합 후 삼항연산자

--- Refactoring/ch-10/03-2.js
+++ Refactoring/ch-10/03-2.js
@@ -1,8 +1,8 @@
 const adjustCapital = (anInstrument) => {
-  if (anInstrument.capital <= 0) return 0;
-  if (anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return 0;
-  return (
-    (anInstrument.income / anInstrument.duration) *
-    anInstrument.adjustmentFactor
-  );
+  return anInstrument.capital <= 0 ||
+    anInstrument.interestRate <= 0 ||
+    anInstrument.duration <= 0
+    ? 0
+    : (anInstrument.income / anInstrument.duration) *
+        anInstrument.adjustmentFactor;
 };
