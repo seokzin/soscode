b0fcca88b023f9519a11074f9bd9ba3973ebe0c0
seokzin <seokzin95@gmail.com>
2022-08-02 23:54:35 +0900

    refac: 조건 검사 로직을 함수로 추출

--- Refactoring/ch-10/05-2.js
+++ Refactoring/ch-10/05-2.js
@@ -1,41 +1,62 @@
-const registry = { billingPlans: { basic: '' } }
+const registry = { billingPlans: { basic: "" } };
+
+const createUnknownCustomer = () => ({
+  isUnknown: true,
+});
+
+const isUnknown = (arg) => arg.isUnknown;
+
 class Site {
-  _customer
+  _customer;
   get customer() {
-    return this._customer
+    return this._customer === "미확인 고객"
+      ? createUnknownCustomer()
+      : this._customer;
+  }
+  get isUnknown() {
+    return false;
   }
 }
 
 class Customer {
-  _name
-  _billingPlan
-  _paymentHistory
+  _name;
+  _billingPlan;
+  _paymentHistory;
+
   get name() {
-    return this._name
+    return this._name;
   }
+
   get billingPlan() {
-    return this._billingPlan
+    return this._billingPlan;
   }
+
   set billingPlan(arg) {
-    this._billingPlan = arg
+    this._billingPlan = arg;
   }
+
   get paymentHistory() {
-    return this._paymentHistory
+    return this._paymentHistory;
   }
 }
 
 const client1 = () => {
-  const customer = new Site().customer
-  //...
-  let customerName
-  if (customer === '미확인 고객') customerName = '거주자'
-  else customerName = customer.name
-}
+  const customer = new Site().customer;
+  let customerName;
+  if (isUnknown(customer)) customerName = "거주자";
+  else customerName = customer.name;
+};
+
 const client2 = () => {
-  const customer = new Site().customer
-  const plan = customer === '미확인 고객' ? registry.billingPlans.basic : customer.billingPlan
-}
+  const customer = new Site().customer;
+  const plan = isUnknown(customer)
+    ? registry.billingPlans.basic
+    : customer.billingPlan;
+};
+
 const client3 = () => {
-  const customer = new Site().customer
-  const weeksDelinquent = customer === '미확인 고객' ? 0 : customer.paymentHsitry.weeksDelinquentInLastYear
-}
+  const customer = new Site().customer;
+  const weeksDelinquent = isUnknown(customer)
+    ? 0
+    : customer.paymentHsitry.weeksDelinquentInLastYear;
+};
