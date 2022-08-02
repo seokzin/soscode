1181d747ab31cca2308316ccabc0479d957c1d70
seokzin <seokzin95@gmail.com>
2022-08-02 23:35:22 +0900

    refac: super 키워드를 활용한 get 오버라이드

--- Refactoring/ch-10/04-2.js
+++ Refactoring/ch-10/04-2.js
@@ -62,8 +62,12 @@ class ExperiencedChinaRating extends Rating {
     return this.history.length > 10 ? 1 : 0;
   }
 
+  get voyageProfitFactor() {
+    return super.voyageProfitFactor + 3;
+  }
+
   get voyageLengthFactor() {
-    let result = 3;
+    let result = 0;
     if (this.voyage.length > 12) result += 1;
     if (this.voyage.length > 18) result -= 1;
     return result;
