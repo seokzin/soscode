50fc3f36270d022d53f8a37f1ab4bd3a3858f1cd
seokzin <seokzin95@gmail.com>
2022-08-02 23:00:36 +0900

    refac: 조류별 서브 클래스 생성
    - 클래스 생성을 위한 팩토리 함수

--- Refactoring/ch-10/04-1.js
+++ Refactoring/ch-10/04-1.js
@@ -2,47 +2,71 @@ const feathers = (birds) => new Map(birds.map((b) => [b.name, feather(b)]));
 const velocities = (birds) => new Map(birds.map((b) => [b.name, velocity(b)]));
 
 class Bird {
-  type;
-  numberOfCoconuts;
-  voltage;
-  isNailed;
   constructor(bird) {
     Object.assign(this, bird);
   }
 
   get feather() {
-    switch (this.type) {
-      case "유럽 제비":
-        return "보통";
-      case "아프리카 제비":
-        return this.numberOfCoconuts > 2 ? "지침" : "보통";
-      case "노르웨이 파랑 앵무":
-        return this.voltage > 100 ? "그을림" : "예쁨";
-      default:
-        return "알수없음";
-    }
+    return "알수없음";
   }
 
   get velocity() {
-    switch (this.type) {
-      case "유럽 제비":
-        return 35;
-      case "아프리카 제비":
-        return 40 - 2 * this.numberOfCoconuts;
-      case "노르웨이 파랑 앵무":
-        return this.isNailed ? 0 : 10 + this.voltage / 10;
-      default:
-        return null;
-    }
+    return null;
   }
 }
 
+class EuropeanSwallow extends Bird {
+  get feather() {
+    return "보통";
+  }
+
+  get velocity() {
+    return 35;
+  }
+}
+
+class AfricaSwallow extends Bird {
+  numberOfCoconuts;
+  get feather() {
+    return this.numberOfCoconuts > 2 ? "지침" : "보통";
+  }
+
+  get velocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
+}
+
+class NorwegianBlueParrot extends Bird {
+  voltage;
+  isNailed;
+  get feather() {
+    return this.voltage > 100 ? "그을림" : "예쁨";
+  }
+
+  get velocity() {
+    return this.isNailed ? 0 : 10 + this.voltage / 10;
+  }
+}
+
+const createBird = (bird) => {
+  switch (bird.type) {
+    case "유럽 제비":
+      return new EuropeanSwallow(bird);
+    case "아프리카 제비":
+      return new AfricaSwallow(bird);
+    case "노르웨이 파랑 앵무":
+      return new NorwegianBlueParrot(bird);
+    default:
+      return new Bird(bird);
+  }
+};
+
 const feather = (bird) => {
-  return new Bird(bird).feather;
+  return createBird(bird).feather;
 };
 
 const velocity = (bird) => {
-  return new Bird(bird).velocity;
+  return createBird(bird).velocity;
 };
 
 const birds = [
