3c0684a9b47b80df7f8b85aa69201d11fc61543f
seokzin <seokzin95@gmail.com>
2022-08-03 22:59:06 +0900

    refac: 인자에 객체 통쨰로 넘기기

--- Refactoring/ch-11/04-1.js
+++ Refactoring/ch-11/04-1.js
@@ -1,37 +1,42 @@
 class TemperatureRange {
-  high
-  low
+  high;
+  low;
   constructor(low, high) {
-    this.high = high
-    this.low = low
+    this.high = high;
+    this.low = low;
   }
 }
+
 class Room {
-  daysTempRange
+  daysTempRange;
   constructor(min, max) {
-    this.daysTempRange = new TemperatureRange(min, max)
+    this.daysTempRange = new TemperatureRange(min, max);
   }
 }
-const room = new Room(22, 24)
 
 class HeatingPlan {
-  _temperatureRange
+  _temperatureRange;
   constructor(low, high) {
-    this._temperatureRange = new TemperatureRange(low, high)
+    this._temperatureRange = new TemperatureRange(low, high);
   }
-  withinRange(bottom, top) {
-    return bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
+
+  withinRange(numberRange) {
+    return (
+      numberRange.low >= this._temperatureRange.low &&
+      numberRange.high <= this._temperatureRange.high
+    );
   }
 }
 
 const client = () => {
-  const plan = new HeatingPlan(21, 25)
-  const low = room.daysTempRange.low
-  const high = room.daysTempRange.high
-  if (!plan.withinRange(low, high)) {
-    console.log('방 온도가 지정 범위를 벗어났습니다.')
+  const plan = new HeatingPlan(21, 25);
+  const room = new Room(22, 24);
+
+  if (!plan.withinRange(room.daysTempRange)) {
+    console.log("방 온도가 지정 범위를 벗어났습니다.");
   } else {
-    console.log('적정 온도입니다.')
+    console.log("적정 온도입니다.");
   }
-}
-client()
+};
+
+client();
