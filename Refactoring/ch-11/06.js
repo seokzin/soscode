24140d65cb746fec014af20995d7142d5aae6756
seokzin <seokzin95@gmail.com>
2022-08-03 23:25:31 +0900

    refac: 메서드 매개변수화

--- Refactoring/ch-11/06.js
+++ Refactoring/ch-11/06.js
@@ -1,25 +1,30 @@
-const thermostat = {
-  selectedTemperature: 25,
-  currentTemperature: 27,
-}
-
 class HeatingPlan {
-  #max
-  #min
-  get targetTemperature() {
-    if (thermostat.selectedTemperature > this.#max) return this.#max
-    else if (thermostat.selectedTemperature < this.#min) return this.#min
-    else return thermostat.selectedTemperature
+  #max;
+  #min;
+
+  targetTemperature(selectedTemperature) {
+    if (selectedTemperature > this.#max) return this.#max;
+    else if (selectedTemperature < this.#min) return this.#min;
+    else return selectedTemperature;
   }
 }
 
 const temperatureController = () => {
-  const setToHeat = () => {}
-  const setToCool = () => {}
-  const setOff = () => {}
+  const setToHeat = () => {};
+  const setToCool = () => {};
+  const setOff = () => {};
 
-  const heatingPlan = new HeatingPlan()
-  if (heatingPlan.targetTemperature > thermostat.currentTemperature) setToHeat()
-  else if (heatingPlan.targetTemperature < thermostat.currentTemperature) setToCool()
-  else setOff()
-}
+  const thermostat = {
+    selectedTemperature: 25,
+    currentTemperature: 27,
+  };
+
+  const heatingPlan = new HeatingPlan();
+  if (heatingPlan.targetTemperature(thermostat.selectedTemperature) > thermostat.currentTemperature)
+    setToHeat();
+  else if (
+    heatingPlan.targetTemperature(thermostat.selectedTemperature) < thermostat.currentTemperature
+  )
+    setToCool();
+  else setOff();
+};
