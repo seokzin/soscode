3d70fef44e47f20aeed17e0eb1113b48c4c40e87
seokzin <seokzin95@gmail.com>
2022-08-03 23:41:32 +0900

    refac: 함수를 메서드로 캡슐화
    - 이를 '명령 객체' 또는 '명령'이라 부름
    - 명령 객체 : 그 함수만을 위한 객체로 캡슐화 한 것

--- Refactoring/ch-11/09.js
+++ Refactoring/ch-11/09.js
@@ -1,20 +1,36 @@
-const score = (candidate, medicalExam, scoringGuide) => {
-  let result = 0
-  let healthLevel = 0
-  let highMedicalRiskFlag = false
-  if (medicalExam.isSmoker) {
-    healthLevel += 10
-    highMedicalRiskFlag = true
+class Score {
+  #candidate;
+  #medicalExam;
+  #scoringGuide;
+  constructor(candidate, medicalExam, scoringGuide) {
+    this.#candidate = candidate;
+    this.#medicalExam = medicalExam;
+    this.#scoringGuide = scoringGuide;
   }
-  let certificationGrade = 'regular'
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = 'low'
-    result -= 5
+
+  exec() {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+    if (this.#medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMedicalRiskFlag = true;
+    }
+    let certificationGrade = "regular";
+    if (this.#scoringGuide.stateWithLowCertification(this.#candidate.originState)) {
+      certificationGrade = "low";
+      result -= 5;
+    }
+    result -= Math.max(healthLevel - 5, 0);
+    return { result, certificationGrade, highMedicalRiskFlag };
   }
-  result -= Math.max(healthLevel - 5, 0)
-  return { result, certificationGrade, highMedicalRiskFlag }
 }
 
-const scoringGuide = { stateWithLowCertification: state => state === 'CA' || state === 'ME' }
-console.log(score({ originState: 'CA' }, { isSmoker: true }, scoringGuide))
-console.log(score({ originState: 'NY' }, { isSmoker: false }, scoringGuide))
+const score = (candidate, medicalExam, scoringGuide) => {
+  return new Score(candidate, medicalExam, scoringGuide).exec();
+};
+
+const scoringGuide = { stateWithLowCertification: (state) => state === "CA" || state === "ME" };
+
+console.log(score({ originState: "CA" }, { isSmoker: true }, scoringGuide));
+console.log(score({ originState: "NY" }, { isSmoker: false }, scoringGuide));
