996b33fb78730032eb5c72821f1eb8872d7a679f
seokzin <seokzin95@gmail.com>
2022-08-04 00:12:42 +0900

    refac: 예외 처리를 조건 검사로 예방
    - 예측 가능한 동작은 예외 처리보다 예방이 낫다

--- Refactoring/ch-11/13.js
+++ Refactoring/ch-11/13.js
@@ -1,47 +1,39 @@
 const Resource = (() => {
-  let id = 0
+  let id = 0;
   return class Resource {
-    id
+    id;
     constructor() {
-      id += 1
-      this.id = id
+      id += 1;
+      this.id = id;
     }
     static create() {
-      return new Resource()
+      return new Resource();
     }
-  }
-})()
+  };
+})();
 
 class ResourcePool {
-  available = []
-  allocated = new Set()
-  get() {
-    let result
-    try {
-      result = this.available.pop()
-      if (!result) throw Error('no available resource')
-      this.allocated.add(result)
-    } catch (e) {
-      result = Resource.create()
-      this.allocated.add(result)
-    }
-    return result
+  available = [];
+  allocated = new Set();
+
+  isEmpty() {
+    return this.available.length === 0;
   }
+
+  get() {
+    let result;
+
+    if (this.isEmpty()) {
+      result = this.available.pop();
+      this.allocated.add(result);
+    } else {
+      result = Resource.create();
+      this.allocated.add(result);
+    }
+    return result;
+  }
+
   add() {
-    this.available.push(Resource.create())
+    this.available.push(Resource.create());
   }
 }
-
-const pool = new ResourcePool()
-pool.get()
-console.log({ available: pool.available, allocated: pool.allocated })
-pool.add()
-pool.add()
-pool.add()
-console.log({ available: pool.available, allocated: pool.allocated })
-pool.get()
-pool.get()
-pool.get()
-console.log({ available: pool.available, allocated: pool.allocated })
-pool.get()
-console.log({ available: pool.available, allocated: pool.allocated })
