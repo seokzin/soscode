c4770ee47833c708e21ba6790eb5eef77346b642
seokzin <seokzin95@gmail.com>
2022-08-03 23:49:46 +0900

    refac: 팩토리 함수 선언

--- Refactoring/ch-11/10.js
+++ Refactoring/ch-11/10.js
@@ -1,22 +1,28 @@
 class ChargeCalculator {
-  #customer
-  #usage
-  #provider
+  #customer;
+  #usage;
+  #provider;
   constructor(customer, usage, provider) {
-    this.#customer = customer
-    this.#usage = usage
-    this.#provider = provider
+    this.#customer = customer;
+    this.#usage = usage;
+    this.#provider = provider;
   }
+
   get baseCharge() {
-    return this.#customer.baseRate * this.#usage
+    return this.#customer.baseRate * this.#usage;
   }
+
   get charge() {
-    return this.baseCharge + this.#provider.connectionCharge
+    return this.baseCharge + this.#provider.connectionCharge;
   }
 }
 
-const customer = { baseRate: 100 }
-const usage = 1000
-const provider = { connectionCharge: 50 }
-const monthCharge = new ChargeCalculator(customer, usage, provider).charge
-console.log(monthCharge)
+const charge = (customer, usage, provider) =>
+  new ChargeCalculator(customer, usage, provider).charge;
+
+const customer = { baseRate: 100 };
+const usage = 1000;
+const provider = { connectionCharge: 50 };
+const monthCharge = charge(customer, usage, provider);
+
+console.log(monthCharge);
