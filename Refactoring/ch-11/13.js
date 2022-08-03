b2dc79e9e0a01b52d9141485ebcc6cffcfaebf99
seokzin <seokzin95@gmail.com>
2022-08-04 00:15:24 +0900

    refac: 메서드 간소화
    - 조건절 중복 로직 추출
    - 변수 인라인화

--- Refactoring/ch-11/13.js
+++ Refactoring/ch-11/13.js
@@ -16,20 +16,9 @@ class ResourcePool {
   available = [];
   allocated = new Set();
 
-  isEmpty() {
-    return this.available.length === 0;
-  }
-
   get() {
-    let result;
-
-    if (this.isEmpty()) {
-      result = this.available.pop();
-      this.allocated.add(result);
-    } else {
-      result = Resource.create();
-      this.allocated.add(result);
-    }
+    const result = this.available.length === 0 ? Resource.create() : this.available.pop();
+    this.allocated.add(result);
     return result;
   }
 
