3204a9247c11321981e8a3edab321cb4ad5df1c9
seokzin <seokzin95@gmail.com>
2022-08-03 22:24:54 +0900

    refac: 비순수함수 분리

--- Refactoring/ch-11/01.js
+++ Refactoring/ch-11/01.js
@@ -1,17 +1,41 @@
 const setOffAlarms = () => {
-  console.warn('악당을 찾았소')
-}
-const alertForMiscreant = people => {
+  console.warn("악당을 찾았소");
+};
+
+const findMiscreant = (people) => {
   for (const p of people) {
-    if (p === '조커') {
-      setOffAlarms()
-      return '조커'
+    if (p === "조커") {
+      return "조커";
     }
-    if (p === '사루만') {
-      setOffAlarms()
-      return '사루만'
+    if (p === "사루만") {
+      return "사루만";
     }
   }
-  return ''
-}
-alertForMiscreant(['슈퍼맨', '배트맨', '아이언맨', '사루만', '블랙위도우', '조커', '스파이더맨'])
+  return "";
+};
+
+const alertForMiscreant = (people) => {
+  for (const p of people) {
+    if (p === "조커") {
+      setOffAlarms();
+    }
+    if (p === "사루만") {
+      setOffAlarms();
+    }
+  }
+  return;
+};
+
+const people = [
+  "슈퍼맨",
+  "배트맨",
+  "아이언맨",
+  "사루만",
+  "블랙위도우",
+  "조커",
+  "스파이더맨",
+];
+
+const found = findMiscreant(people);
+
+alertForMiscreant(people);
