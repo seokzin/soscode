1ef9f49d3691d15bff1be7810c6182b2553add7c
seokzin <seokzin95@gmail.com>
2022-08-03 23:15:56 +0900

    refac: 게터 메서드 추출로 로직 간소화

--- Refactoring/ch-11/05.js
+++ Refactoring/ch-11/05.js
@@ -1,22 +1,25 @@
 class Order {
-  quantity
-  itemPrice
+  quantity;
+  itemPrice;
   constructor() {}
+
   get finalPrice() {
-    const basePrice = this.quantity * this.itemPrice
-    let discountLevel
-    if (this.quantity > 100) discountLevel = 2
-    else discountLevel = 1
-    return this.discountedPrice(basePrice, discountLevel)
+    const basePrice = this.quantity * this.itemPrice;
+    return this.discountedPrice(basePrice);
   }
-  discountedPrice(basePrice, discountLevel) {
-    switch (discountLevel) {
+
+  get discountLevel() {
+    return this.quantity > 100 ? 2 : 1;
+  }
+
+  discountedPrice(basePrice) {
+    switch (this.discountLevel) {
       case 1:
-        return basePrice * 0.95
+        return basePrice * 0.95;
       case 2:
-        return basePrice * 0.9
+        return basePrice * 0.9;
       default:
-        return basePrice
+        return basePrice;
     }
   }
 }
