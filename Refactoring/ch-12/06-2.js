c92ab3f000e3545a283dc7a6df15688059277abc
seokzin <seokzin95@gmail.com>
2022-08-04 23:20:56 +0900

    refac: 타입 변수를 서브 클래스로 변경
    - 6-1처럼 개별 타입이 아닌 타입 자체를 변경

--- Refactoring/ch-12/06-2.js
+++ Refactoring/ch-12/06-2.js
@@ -1,29 +1,50 @@
 class Employee {
-  #name
-  #type
+  #name;
+  #type;
   constructor(name, type) {
-    this.validateType(type)
-    this.#name = name
-    this.#type = type
+    this.validateType(type);
+    this.#name = name;
+    this.#type = type;
   }
+
   validateType(arg) {
-    if (!['engineer', 'manager', 'salesperson'].includes(arg)) throw new Error(`${arg}라는 직원 유형은 없습니다.`)
+    if (!["engineer", "manager", "salesperson"].includes(arg))
+      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
   }
+
+  get typeString() {
+    return this.#type.toString();
+  }
+
   get type() {
-    return this.#type
+    return this.#type;
   }
+
   set type(arg) {
-    this.#type = arg
+    this.#type = arg;
   }
+
   get capitalizedType() {
-    return this.#type.charAt(0).toUpperCase() + this.#type.slice(1).toLowerCase()
+    return this.#type.charAt(0).toUpperCase() + this.#type.slice(1).toLowerCase();
   }
+
   toString() {
-    return `${this.#name} is a ${this.capitalizedType}`
+    return `${this.#name} is a ${this.capitalizedType}`;
   }
 }
 
-console.log(new Employee('roy', 'engineer').toString())
-console.log(new Employee('jay', 'manager').toString())
-console.log(new Employee('kay', 'salesperson').toString())
-console.log(new Employee('tei', 'nobody').toString())
+class EmployeeType {
+  _value;
+  constructor(str) {
+    this._value = str;
+  }
+
+  toString() {
+    return this._value;
+  }
+}
+
+console.log(new Employee("roy", "engineer").toString());
+console.log(new Employee("jay", "manager").toString());
+console.log(new Employee("kay", "salesperson").toString());
+// console.log(new Employee("tei", "nobody").toString());
