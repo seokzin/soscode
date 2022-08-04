d006e286bf0936da9984d560e2bad8c57f457db7
seokzin <seokzin95@gmail.com>
2022-08-05 00:39:51 +0900

    refac: 팩토리 함수 선언

--- Refactoring/ch-12/10-1.js
+++ Refactoring/ch-12/10-1.js
@@ -1,72 +1,86 @@
-import dayjs from 'dayjs'
+import dayjs from "dayjs";
 
 class Booking {
-  #show
-  #date
+  #show;
+  #date;
   constructor(show, date) {
-    this.#show = show
-    this.#date = date
+    this.#show = show;
+    this.#date = date;
   }
+
   get date() {
-    return this.#date
+    return this.#date;
   }
+
   get show() {
-    return this.#show
+    return this.#show;
   }
+
   get hasTalkback() {
-    return this.show.hasOwnProperty('talkback') && !this.isPeakDay
+    return this.show.hasOwnProperty("talkback") && !this.isPeakDay;
   }
+
   get basePrice() {
-    let result = this.show.price
-    if (this.isPeakDay) result += Math.round(result * 0.15)
-    return result
+    let result = this.show.price;
+    if (this.isPeakDay) result += Math.round(result * 0.15);
+    return result;
   }
+
   get isPeakDay() {
-    return this.date.isAfter(dayjs('2021-07-15')) && this.date.isBefore(dayjs('2021-07-31'))
+    return this.date.isAfter(dayjs("2021-07-15")) && this.date.isBefore(dayjs("2021-07-31"));
   }
 }
 
 class PremiumBooking extends Booking {
-  #extras
+  #extras;
   constructor(show, date, extras) {
-    super(show, date)
-    this.#extras = extras
+    super(show, date);
+    this.#extras = extras;
   }
+
   get hasTalkback() {
-    return this.show.hasOwnProperty('talkback')
+    return this.show.hasOwnProperty("talkback");
   }
+
   get basePrice() {
-    return Math.round(super.basePrice + this.#extras.premiumFee)
+    return Math.round(super.basePrice + this.#extras.premiumFee);
   }
+
   get hasDinner() {
-    return this.#extras.hasOwnProperty('dinner') && !this.isPeakDay
+    return this.#extras.hasOwnProperty("dinner") && !this.isPeakDay;
   }
 }
 
-const booking = new Booking({ price: 100, talkback: true }, dayjs('2021-07-11'))
-const premiumBooking1 = new PremiumBooking({ price: 100, talkback: true }, dayjs('2021-07-13'), {
+const createBooking = (show, date) => new Booking(show, date);
+const createPremiumBooking = (show, date, extras) => new PremiumBooking(show, date, extras);
+
+const booking = createBooking({ price: 100, talkback: true }, dayjs("2021-07-11"));
+const premiumBooking1 = createPremiumBooking({ price: 100, talkback: true }, dayjs("2021-07-13"), {
   dinner: true,
   premiumFee: 10,
-})
-const premiumBooking2 = new PremiumBooking({ price: 100 }, dayjs('2021-07-17'), {
+});
+const premiumBooking2 = createPremiumBooking({ price: 100 }, dayjs("2021-07-17"), {
   dinner: true,
   premiumFee: 10,
-})
+});
+
 console.log({
   price: booking.basePrice,
   dinner: booking.hasDinner,
   talkback: booking.hasTalkback,
   peakDay: booking.isPeakDay,
-})
+});
+
 console.log({
   price: premiumBooking1.basePrice,
   dinner: premiumBooking1.hasDinner,
   talkback: premiumBooking1.hasTalkback,
   peakDay: premiumBooking1.isPeakDay,
-})
+});
+
 console.log({
   price: premiumBooking2.basePrice,
   dinner: premiumBooking2.hasDinner,
   talkback: premiumBooking2.hasTalkback,
   peakDay: premiumBooking2.isPeakDay,
-})
+});
