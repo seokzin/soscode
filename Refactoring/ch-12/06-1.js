668d0a4d094ae121e500b7c0f029796c93ddecab
seokzin <seokzin95@gmail.com>
2022-08-04 23:08:27 +0900

    refac: 타입 변수를 서브 클래스로 변경
    - 서브 클래스는 다형성의 장점을 가질 수 있음

--- Refactoring/ch-12/06-1.js
+++ Refactoring/ch-12/06-1.js
@@ -1,19 +1,58 @@
 class Employee {
-  #name
-  #type
+  #name;
+  #type;
   constructor(name, type) {
-    this.validateType(type)
-    this.#name = name
-    this.#type = type
+    this.validateType(type);
+    this.#name = name;
+    this.#type = type;
   }
+
   validateType(arg) {
-    if (!['engineer', 'manager', 'salesperson'].includes(arg)) throw new Error(`${arg}라는 직원 유형은 없습니다.`)
+    if (!["engineer", "manager", "salesperson"].includes(arg))
+      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
   }
+
+  get type() {
+    return this.#type;
+  }
+
   toString() {
-    return `${this.#name} is a ${this.#type}`
+    return `${this.#name} is a ${this.type}`;
   }
 }
-const roy = new Employee('roy', 'engineer')
-const jay = new Employee('jay', 'manager')
-const kay = new Employee('kay', 'salesperson')
-const tei = new Employee('tei', 'nobody')
+
+class Engineer extends Employee {
+  get type() {
+    return "engineer";
+  }
+}
+
+class Salesperson extends Employee {
+  get type() {
+    return "salesperson";
+  }
+}
+
+class Manager extends Employee {
+  get type() {
+    return "manager";
+  }
+}
+
+const createEmployee = (name, type) => {
+  switch (type) {
+    case "engineer":
+      return new Engineer(name, type);
+    case "salesperson":
+      return new Salesperson(name, type);
+    case "manager":
+      return new Manager(name, type);
+    default:
+      return new Employee(name, type);
+  }
+};
+
+const roy = createEmployee("roy", "engineer");
+const jay = createEmployee("jay", "manager");
+const kay = createEmployee("kay", "salesperson");
+const tei = createEmployee("tei", "nobody");
