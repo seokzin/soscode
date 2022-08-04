e905f9655e265905f946d04c40fc5c457ed33b19
seokzin <seokzin95@gmail.com>
2022-08-04 23:53:36 +0900

    refac: name 필드 슈퍼클래스로 추출

--- Refactoring/ch-12/08.js
+++ Refactoring/ch-12/08.js
@@ -1,54 +1,66 @@
-class Employee {
-  #name
-  #id
-  #monthlyCost
+class Party {
+  _name;
+  constructor(name) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+}
+
+class Employee extends Party {
+  _name;
+  #id;
+  #monthlyCost;
   constructor(name, id, monthlyCost) {
-    this.#name = name
-    this.#id = id
-    this.#monthlyCost = monthlyCost
+    super(name);
+    this.#id = id;
+    this.#monthlyCost = monthlyCost;
   }
+
   get monthlyCost() {
-    return this.#monthlyCost
+    return this.#monthlyCost;
   }
-  get name() {
-    return this.#name
-  }
+
   get id() {
-    return this.#id
+    return this.#id;
   }
+
   get annualCost() {
-    return this.#monthlyCost * 12
+    return this.#monthlyCost * 12;
   }
 }
 
-class Department {
-  #name
-  #staff
+class Department extends Party {
+  _name;
+  #staff;
   constructor(name, staff) {
-    this.#name = name
-    this.#staff = staff
+    super(name);
+    this.#staff = staff;
   }
-  get name() {
-    return this.#name
-  }
+
   get staff() {
-    return this.#staff
+    return this.#staff;
   }
+
   get totalMonthlyCost() {
-    return this.#staff.map(e => e.monthlyCost).reduce((sum, cost) => sum + cost, 0)
+    return this.#staff.map((e) => e.monthlyCost).reduce((sum, cost) => sum + cost, 0);
   }
+
   get headCount() {
-    return this.staff.length
+    return this.staff.length;
   }
+
   get totalAnnualCost() {
-    return this.totalMonthlyCost * 12
+    return this.totalMonthlyCost * 12;
   }
 }
 
-const roy = new Employee('Roy', '123', 100)
-const jay = new Employee('Jay', '456', 200)
-const sales = new Department('Sales', [roy, jay])
+const roy = new Employee("Roy", "123", 100);
+const jay = new Employee("Jay", "456", 200);
+const sales = new Department("Sales", [roy, jay]);
 
-console.log(roy.annualCost)
-console.log(jay.annualCost)
-console.log(sales.totalAnnualCost)
+console.log(roy.annualCost);
+console.log(jay.annualCost);
+console.log(sales.totalAnnualCost);
