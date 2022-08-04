7fac63be67b09a5dc6b022352706b140c6050ac4
seokzin <seokzin95@gmail.com>
2022-08-04 22:52:32 +0900

    refac: 메서드 올리기

--- Refactoring/ch-12/01.js
+++ Refactoring/ch-12/01.js
@@ -1,14 +1,9 @@
 class Party {
-  monthlyCost
+  get annualCost() {
+    return this.monthlyCost * 12;
+  }
 }
 
-class Employee extends Party {
-  get annualCost() {
-    return this.monthlyCost * 12
-  }
-}
-class Department extends Party {
-  get totalAnnualCost() {
-    return this.monthlyCost * 12
-  }
-}
+class Employee extends Party {}
+
+class Department extends Party {}
