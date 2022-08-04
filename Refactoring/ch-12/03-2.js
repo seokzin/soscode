b3bf54812acc9ac6ab3115ccc93709e2af6327db
seokzin <seokzin95@gmail.com>
2022-08-04 23:03:02 +0900

    refac: 메서드 추출 후 올리기

--- Refactoring/ch-12/03-2.js
+++ Refactoring/ch-12/03-2.js
@@ -1,40 +1,44 @@
 class Employee {
-  #name
+  #name;
   constructor(name) {
-    this.#name = name
+    this.#name = name;
   }
+
   get isPrivileged() {
-    return false
+    return false;
   }
+
   assignCar() {
-    console.log(this.#name, 'car assigned')
+    console.log(this.#name, "car assigned");
+  }
+
+  finishConstruction() {
+    if (this.isPrivileged) this.assignCar();
   }
 }
 
 class Manager extends Employee {
-  #grade
+  #grade;
   constructor(name, grade) {
-    super(name)
-    this.#grade = grade
-    if (this.isPrivileged) this.assignCar()
+    super(name);
+    this.#grade = grade;
+    this.finishConstruction();
   }
+
   get isPrivileged() {
-    return this.#grade > 4
+    return this.#grade > 4;
   }
 }
 
 class Producer extends Employee {
-  #grade
+  #grade;
   constructor(name, grade) {
-    super(name)
-    this.#grade = grade
-    if (this.isPrivileged) this.assignCar()
-  }
-  get isPrivileged() {
-    return this.#grade > 4
+    super(name);
+    this.#grade = grade;
+    this.finishConstruction();
   }
 }
 
-const roy = new Employee('로이')
-const jay = new Manager('제이', 5)
-const kay = new Producer('케이', 6)
+const roy = new Employee("로이");
+const jay = new Manager("제이", 5);
+const kay = new Producer("케이", 6);
