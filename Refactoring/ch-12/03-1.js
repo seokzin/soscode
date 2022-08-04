23842f10b22466862af2e6808786b33f8fc3eeb0
seokzin <seokzin95@gmail.com>
2022-08-04 22:59:37 +0900

    refac: 생성자 올리기

--- Refactoring/ch-12/03-1.js
+++ Refactoring/ch-12/03-1.js
@@ -1,35 +1,41 @@
-class Party {}
+class Party {
+  _name;
+  constructor(name) {
+    this._name = name;
+  }
+}
 
 export class Employee extends Party {
-  #name
-  #id
-  #monthlyCost
+  #id;
+  #monthlyCost;
   constructor(name, id, monthlyCost) {
-    super()
-    this.#name = name
-    this.#id = id
-    this.#monthlyCost = monthlyCost
+    super(name);
+    this.#id = id;
+    this.#monthlyCost = monthlyCost;
   }
+
   get name() {
-    return this.#name
+    return this._name;
   }
+
   get monthlyCost() {
-    return this.#monthlyCost
+    return this.#monthlyCost;
   }
 }
 
 export class Department extends Party {
-  #name
-  #staff
+  #staff;
   constructor(name, staff) {
-    super()
-    this.#name = name
-    this.#staff = staff
+    super(name);
+    this._name = name;
+    this.#staff = staff;
   }
+
   get name() {
-    return this.#name
+    return this._name;
   }
+
   get staff() {
-    return this.#staff
+    return this.#staff;
   }
 }
