6922c60ad2007d29f992dc4bf1df0903c9bba7d6
seokzin <seokzin95@gmail.com>
2022-08-04 23:36:37 +0900

    refac: 팩토리 함수 선언

--- Refactoring/ch-12/07.js
+++ Refactoring/ch-12/07.js
@@ -1,26 +1,41 @@
 class Person {
-  #name
+  #name;
   constructor(name) {
-    this.#name = name
+    this.#name = name;
   }
+
   get name() {
-    return this.#name
+    return this.#name;
   }
+
   get genderCode() {
-    return 'X'
-  }
-}
-class Male extends Person {
-  get genderCode() {
-    return 'M'
-  }
-}
-class Female extends Person {
-  get genderCode() {
-    return 'F'
+    return "X";
   }
 }
 
-const people = [new Male('재남'), new Female('지금'), new Male('로이'), new Female('이지')]
-const numberOfMales = people.filter(p => p instanceof Male).length
-console.log(numberOfMales)
+class Male extends Person {
+  get genderCode() {
+    return "M";
+  }
+}
+
+class Female extends Person {
+  get genderCode() {
+    return "F";
+  }
+}
+
+const createPerson = (record) => {
+  switch (record.gender) {
+    case "M":
+      return Male(record.name);
+    case "F":
+      return Female(record.name);
+    default:
+      return Person(record.name);
+  }
+};
+
+const people = [new Male("재남"), new Female("지금"), new Male("로이"), new Female("이지")];
+const numberOfMales = people.filter((p) => p instanceof Male).length;
+console.log(numberOfMales);
