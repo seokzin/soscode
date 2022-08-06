cf92e8a56c18b89dbca744581f2c08ad356a96bf
seokzin <seokzin95@gmail.com>
2022-08-06 19:14:51 +0900

    feat: join 함수 구현
    - reduce + curry 조합으로 기존 join보다 범용적
    - iter에 대해 사용 가능

--- FP/queryStr.js
+++ FP/queryStr.js
@@ -1,12 +1,12 @@
 import { log } from './log.js';
 import { pipe } from './pipe.js';
 import { map } from './map.js';
-import { reduce } from './reduce.js';
+import { join } from './join.js';
 
 export const queryStr = pipe(
   Object.entries,
   map(([k, v]) => `${k}=${v}`),
-  reduce((a, b) => `${a}&${b}`)
+  join('&')
 );
 
-// log(queryStr({ limit: 10, offset: 10, type: 'notice' }));
+log(queryStr({ limit: 10, offset: 10, type: 'notice' }));
