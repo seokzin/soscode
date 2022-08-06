d2988a1f966ad476faa1c4bcb15488c39a914675
seokzin <seokzin95@gmail.com>
2022-08-06 22:55:16 +0900

    feat: flatten 함수 구현
    - 1 depth만 평탄화 가능

---
+++ FP/flatten.js
@@ -0,0 +1,23 @@
+import { log } from './log.js';
+import { take } from './take.js';
+import { pipe } from './pipe.js';
+
+export const l_flatten = function* (iter) {
+  for (const a of iter) {
+    if (a && a[Symbol.iterator]) {
+      for (const b of a) {
+        yield b;
+      }
+    } else {
+      yield a;
+    }
+  }
+};
+
+export const flatten = pipe(l_flatten, take(Infinity));
+
+// const l = l_flatten([[1, 2, 3], 4, 5, [6, 7, 8, 9]]);
+// const a = flatten([[1, 2, 3], 4, 5, [6, 7, 8, 9]]);
+
+// log([...l]);
+// log([...a]);
