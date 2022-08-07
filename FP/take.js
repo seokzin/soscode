46fd4bb495d2c25061fe710d45d92beb7b2b2154
seokzin <seokzin95@gmail.com>
2022-08-07 22:45:51 +0900

    feat: take에서 Promise 처리하도록 변경

--- FP/take.js
+++ FP/take.js
@@ -1,14 +1,24 @@
-import { curry } from "./curry.js";
+import { curry } from './curry.js';
 
 export const take = curry((length, iter) => {
   let res = [];
+  iter = iter[Symbol.iterator]();
 
-  for (const a of iter) {
-    res.push(a);
-    if (res.length === length) return res;
-  }
-
-  return res;
+  return (function recur() {
+    let cur;
+    while (!(cur = iter.next()).done) {
+      const a = cur.value;
+      if (acc instanceof Promise)
+        return acc.then((a) => {
+          res.push(a);
+          if (res.length === l) return res;
+          return recur();
+        });
+      res.push(a);
+      if (res.length === l) return res;
+    }
+    return acc;
+  })();
 });
 
 // console.log(take(3)([1, 2, 3, 4, 5])); // [ 1, 2, 3 ]
