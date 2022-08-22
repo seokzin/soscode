b1e3511e71ea1118f0d97b9aa42f023d44a0dd67
seokzin <seokzin95@gmail.com>
2022-08-22 21:59:53 +0900

    feat: object.entries 구현

---
+++ FP/object/entries.js
@@ -0,0 +1,16 @@
+import { log } from '../../log';
+
+const L = {};
+
+L.entries = function* (obj) {
+  for (const k in obj) yield [k, obj[k]];
+};
+
+const obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+};
+
+const it = L.entries(obj);
