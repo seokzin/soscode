e9c4d740ba50bc91bd4468604361a3f088f11baa
seokzin <seokzin95@gmail.com>
2022-10-21 15:32:41 +0900

    feat: useLocalStorage 훅 구현

---
+++ utils/hooks/useLocalStorage.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+const useLocalStorage = (key: string, initialValue: any) => {
+  const item = localStorage.getItem(key);
+  const [value, setValue] = useState(item ? JSON.parse(item) : initialValue);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;
