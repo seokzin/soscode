91470d75b659fdb64bfbed7fd0545c0d95e1a1c4
seokzin <seokzin95@gmail.com>
2022-10-21 15:48:12 +0900

    feat: useInput 훅 구현

---
+++ utils/hooks/useInput.ts
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+const useInput = (initialValue: string) => {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value: inputValue },
+    } = e;
+
+    setValue(inputValue);
+  };
+
+  return [value, onChange];
+};
