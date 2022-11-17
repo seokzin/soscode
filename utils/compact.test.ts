328a563fc1fe0a4c2b59bc49255a59db5fcabab7
seokzin <seokzin95@gmail.com>
2022-11-18 08:49:05 +0900

    feat: compact 함수 구현

---
+++ utils/compact.test.ts
@@ -0,0 +1,11 @@
+import compact from './compact';
+
+describe('compact >', () => {
+  it('should return an array without falsey values', () => {
+    expect(compact([0, 1, false, 2, '', 3])).toEqual([1, 2, 3]);
+  });
+
+  it('should return an empty array if all values are falsey', () => {
+    expect(compact([0, false, '', undefined, null])).toEqual([]);
+  });
+});
---
+++ utils/compact.ts
@@ -0,0 +1,3 @@
+const compact = (arr: any[]) => arr.filter(Boolean);
+
+export default compact;
